5a42aeba0a2108df180e667c8c68ec1fe46e12ff
nicolan74 <nicola.lanzilotto@frontiere21.it>
2018-06-08 18:39:57 +0200

    added service: update user

--- app.js
+++ app.js
@@ -37,13 +37,16 @@ var request = require('./routes/request');
 
 var group = require('./routes/group');
 
+// new 
+var users = require('./routes/users');
+
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
@@ -63,7 +66,7 @@ app.use(passport.initialize());
 app.use(cors());
 
 
-app.get('/', function(req, res) {
+app.get('/', function (req, res) {
   res.send('Chat21 API index page. See the documentation.');
 });
 
@@ -90,33 +93,33 @@ app.use('/:projectid', projectIdSetter);
 // http://aleksandrov.ws/2013/09/12/restful-api-with-nodejs-plus-mongodb/
 // app.use('/:projectid', [passport.authenticate(['basic','jwt'], { session: false }), validtoken]);
 
-
-app.use('/:projectid/contacts', [passport.authenticate(['basic','jwt'], { session: false }), validtoken], contact);
-app.use('/:projectid/messages',[passport.authenticate(['basic','jwt'], { session: false }), validtoken], message);
+app.use('/users', [passport.authenticate(['basic', 'jwt'], { session: false }), validtoken], users);
+app.use('/:projectid/contacts', [passport.authenticate(['basic', 'jwt'], { session: false }), validtoken], contact);
+app.use('/:projectid/messages', [passport.authenticate(['basic', 'jwt'], { session: false }), validtoken], message);
 app.use('/:projectid/departments', department);
-app.use('/:projectid/faq', [passport.authenticate(['basic','jwt'], { session: false }), validtoken], faq);
-app.use('/:projectid/bots', [passport.authenticate(['basic','jwt'], { session: false }), validtoken], bot);
-app.use('/:projectid/faq_kb', [passport.authenticate(['basic','jwt'], { session: false }), validtoken], faq_kb);
+app.use('/:projectid/faq', [passport.authenticate(['basic', 'jwt'], { session: false }), validtoken], faq);
+app.use('/:projectid/bots', [passport.authenticate(['basic', 'jwt'], { session: false }), validtoken], bot);
+app.use('/:projectid/faq_kb', [passport.authenticate(['basic', 'jwt'], { session: false }), validtoken], faq_kb);
 app.use('/projects', project);
-app.use('/:projectid/people', [passport.authenticate(['basic','jwt'], { session: false }), validtoken], person);
-app.use('/:projectid/project_users', [passport.authenticate(['basic','jwt'], { session: false }), validtoken], project_user);
-app.use('/:projectid/requests', [passport.authenticate(['basic','jwt'], { session: false }), validtoken], request);
+app.use('/:projectid/people', [passport.authenticate(['basic', 'jwt'], { session: false }), validtoken], person);
+app.use('/:projectid/project_users', [passport.authenticate(['basic', 'jwt'], { session: false }), validtoken], project_user);
+app.use('/:projectid/requests', [passport.authenticate(['basic', 'jwt'], { session: false }), validtoken], request);
 
-app.use('/:projectid/groups', [passport.authenticate(['basic','jwt'], { session: false }), validtoken], group);
+app.use('/:projectid/groups', [passport.authenticate(['basic', 'jwt'], { session: false }), validtoken], group);
 
 //app.use('/apps', tenant);
 
 
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   var err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
