030ea14d7a6d64248bd2936760b1f96079c7b309
andrea leo <andrea.leo83@gmail.com>
2018-06-08 10:13:02 +0200

    sent to me email

--- config/email.js
+++ config/email.js
@@ -2,5 +2,6 @@ module.exports = {
   'host':'smtp.mailgun.org',
   'username': 'postmaster@mg.tiledesk.com',
   'from': 'postmaster@mg.tiledesk.com',
-  'bcc': 'tiledesknotification@frontiere21.it',
+  'bcc': 'andrea.leo@frontiere21.it',
+  // 'bcc': 'tiledesknotification@frontiere21.it',
 };
