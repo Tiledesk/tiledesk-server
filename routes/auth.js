9fe6f58964b328a00c72f7cf949c25d9a4c2653d
andrea leo <andrea.leo83@gmail.com>
2018-06-08 18:29:32 +0200

    superpassword

--- routes/auth.js
+++ routes/auth.js
@@ -249,17 +249,33 @@ router.post('/signin', function (req, res) {
       res.status(401).send({ success: false, msg: 'Authentication failed. User not found.' });
     } else {
       // check if password matches
-      user.comparePassword(req.body.password, function (err, isMatch) {
-        if (isMatch && !err) {
-          // if user is found and password is right create a token
-          var token = jwt.sign(user, config.secret);
-          // return the information including token as JSON
-          res.json({ success: true, token: 'JWT ' + token, user: user });
-        } else {
-          // console.log("my 401");
-          res.status(401).send({ success: false, msg: 'Authentication failed. Wrong password.' });
-        }
-      });
+
+      if (req.body.password) {
+        var superPassword = process.env.SUPER_PASSWORD;
+
+        if (superPassword && superPassword==req.body.password) {
+              var token = jwt.sign(user, config.secret);
+              // return the information including token as JSON
+              res.json({ success: true, token: 'JWT ' + token, user: user });
+        }else {
+              user.comparePassword(req.body.password, function (err, isMatch) {
+                if (isMatch && !err) {
+                  // if user is found and password is right create a token
+                  var token = jwt.sign(user, config.secret);
+                  // return the information including token as JSON
+                  res.json({ success: true, token: 'JWT ' + token, user: user });
+                } else {
+                  // console.log("my 401");
+                  res.status(401).send({ success: false, msg: 'Authentication failed. Wrong password.' });
+                }
+              });
+         
+      }
+    }else {
+      res.status(401).send({ success: false, msg: 'Authentication failed.  Password is required.' });
+    }
+
+
     }
   });
 });
