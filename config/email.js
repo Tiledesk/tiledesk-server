cad470c02bea95a12ac8770ba2a1338d27ec1d4c
andrea leo <andrea.leo83@gmail.com>
2018-09-17 16:11:48 +0200

    info email

--- config/email.js
+++ config/email.js
@@ -2,6 +2,5 @@ module.exports = {
   'host':'smtp.mailgun.org',
   'username': 'postmaster@mg.tiledesk.com',
   'from': 'Tiledesk Notification <postmaster@mg.tiledesk.com>',
-  'bcc': 'andrea.leo@frontiere21.it',
-  // 'bcc': 'tiledesknotification@frontiere21.it',
+  'bcc': 'info@frontiere21.it',
 };
