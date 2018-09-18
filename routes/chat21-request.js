8b098561058b52fcf25f04fdf88ce26b7f578b1a
andrea leo <andrea.leo83@gmail.com>
2018-09-18 16:34:52 +0200

    webhook chat21

--- routes/chat21-request.js
+++ routes/chat21-request.js
@@ -23,58 +23,76 @@ router.post('/', function(req, res) {
   // console.log("req.user.id", req.user.id);
 
 
+  if (req.body.event_type == "first-message") {
 
-  // var newRequest = new Request({
-  //   requester_id: req.body.requester_id,
-  //   requester_fullname: req.body.requester_fullname,
-  //   first_text: req.body.first_text,
-  //   support_status: req.body.support_status,
-  //   partecipants: req.body.partecipants,
-  //   departmentid: req.body.departmentid,
+    var message = req.body.data;
+    console.log("chat21 message", message);
 
-  //   // recipient: req.body.recipient,
-  //   // recipientFullname: req.body.recipient_fullname,
-  //   // sender: req.body.sender,
-  //   // senderFullname: req.body.sender_fullname,
-  //   // first_message: first_message,
+    var departmentid = message.attributes.departmentid;
+    console.log("chat21 departmentid", departmentid);
 
-  //   rating: req.body.rating,
-  //   rating_message: req.body.rating_message,
+    var sourcePage = message.attributes.sourcePage;
+    console.log("chat21 sourcePage", sourcePage);
 
-  //   agents: req.body.agents,
-  //   availableAgents: req.body.availableAgents,
-  //   assigned_operator_id:  req.body.assigned_operator_id,
-
-  //   //others
-  //   sourcePage: req.body.sourcePage,
-  //   language: req.body.language,
-  //   userAgent: req.body.userAgent,
-
-  //   //standard
-  //   id_project: req.projectid,
-  //   createdBy: req.user.id,
-  //   updatedBy: req.user.id
-  // });
-
-  // newRequest.save(function(err, savedRequest) {
-  //   if (err) {
-  //     console.log('Error saving object.',err);
-  //     return res.status(500).send({success: false, msg: 'Error saving object.', err:err});
-  //   }
+    var client = message.attributes.client;
+    console.log("chat21 client", client);
 
 
-  //   console.log("savedRequest",savedRequest);
-    
-  //   // console.log("XXXXXXXXXXXXXXXX");
-  //   this.sendEmail(req.projectid, savedRequest);
-     
+    var projectid = message.projectid;
+    console.log("chat21 projectid", projectid);
+
     
 
-  //   res.json(savedRequest);
-    
-  // });
+    var newRequest = new Request({
+      requester_id: message.sender,
+      requester_fullname: message.sender_fullname,
+      first_text: message.text,
+      // support_status: req.body.support_status,
+      // partecipants: req.body.partecipants,
+      departmentid: departmentid,
+  
+      // rating: req.body.rating,
+      // rating_message: req.body.rating_message,
+  
+      // agents: req.body.agents,
+      // availableAgents: req.body.availableAgents,
+      // assigned_operator_id:  req.body.assigned_operator_id,
+  
+      //others
+      sourcePage: sourcePage,
+      language: message.language,
+      userAgent: client,
+  
+      //standard
+      id_project: projectid,
+      createdBy: req.user.id,
+      updatedBy: req.user.id
+    });
+  
+    newRequest.save(function(err, savedRequest) {
+      if (err) {
+        console.log('Error saving object.',err);
+        return res.status(500).send({success: false, msg: 'Error saving object.', err:err});
+      }
+  
+  
+      console.log("savedRequest",savedRequest);
+      
+      // console.log("XXXXXXXXXXXXXXXX");
+      this.sendEmail(req.projectid, savedRequest);
+       
+      
+  
+      res.json(savedRequest);
+      
+    });
 
-  res.json("savedRequest");
+  } else {
+    res.json("Not implemented");
+  }
+
+  
+
 });
 
 
