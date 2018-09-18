bbb8de94ca03edf19560b9112b7ecedfd678ce66
andrea leo <andrea.leo83@gmail.com>
2018-09-18 20:14:00 +0200

    email fix

--- models/requestService.js
+++ models/requestService.js
@@ -1,9 +1,11 @@
 'use strict';
 
-const nodemailer = require('nodemailer');
-var config = require('../config/email');
 var departmentService = require('../models/departmentService');
 var Request = require("../models/request");
+var emailService = require("../models/emailService");
+var Project = require("../models/project");
+var User = require("../models/user");
+
 
 class RequestService {
 
@@ -11,6 +13,8 @@ class RequestService {
 
 
   create(requester_id, requester_fullname, id_project, first_text, departmentid, sourcePage, language, userAgent) {
+
+    var that = this;
 
     return departmentService.getOperators(departmentid, id_project, false).then(function (result) {
 
@@ -56,7 +60,7 @@ class RequestService {
                 console.log("savedRequest",savedRequest);
                 
                 // console.log("XXXXXXXXXXXXXXXX");
-                //this.sendEmail(req.projectid, savedRequest);
+                that.sendEmail(id_project, savedRequest);
                 
                 
             
@@ -76,6 +80,83 @@ class RequestService {
   
 
 
+
+  sendEmail(projectid, savedRequest) {
+    // send email
+    try {
+   
+   
+     Project.findById(projectid, function(err, project){
+       if (err) {
+         console.log(err);
+       }
+   
+       if (!project) {
+         console.log("Project not found", req.projectid);
+       } else {
+         
+         console.log("Project", project);
+   
+   
+                 if (savedRequest.support_status==100) { //POOLED
+                 // throw "ciao";
+                   var allAgents = savedRequest.agents;
+                   console.log("allAgents", allAgents);
+   
+                   allAgents.forEach(project_user => {
+                     console.log("project_user", project_user);
+   
+                     User.findById(project_user.id_user, function (err, user) {
+                       if (err) {
+                         console.log(err);
+                       }
+                       if (!user) {
+                         console.log("User not found", project_user.id_user);
+                       } else {
+                         console.log("User email", user.email);
+                         if (user.emailverified) {
+                           emailService.sendNewPooledRequestNotification(user.email, savedRequest, project);
+                         }else {
+                           console.log("User email not verified", user.email);
+                         }
+                       }
+                     });
+   
+                     
+                   });
+   
+                   }else if (savedRequest.support_status==200) { //ASSIGNED
+                     console.log("assigned_operator_id", savedRequest.assigned_operator_id);
+   
+                     User.findById( savedRequest.assigned_operator_id, function (err, user) {
+                       if (err) {
+                         console.log(err);
+                       }
+                       if (!user) {
+                         console.log("User not found",  savedRequest.assigned_operator_id);
+                       } else {
+                         console.log("User email", user.email);
+                         emailService.sendNewAssignedRequestNotification(user.email, savedRequest, project);
+                       }
+                     });
+   
+                     // emailService.sendNewAssignedRequestNotification(user.email, savedRequest);
+                   }else {
+   
+                   }
+   
+   
+         
+         }
+   
+   });
+   
+   } catch (e) {
+     console.log("Errore sending email", e);
+   }
+   //end send email
+   
+   }
  
 
 
