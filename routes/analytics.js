eff0a265094c3f5d2f992a944e31db0adc4b7148
andrea leo <andrea.leo83@gmail.com>
2018-09-28 10:22:46 +0200

    sort fix

--- routes/analytics.js
+++ routes/analytics.js
@@ -49,7 +49,7 @@ var mongoose = require('mongoose');
               "_id":{"year":"$year", "month":"$month", "day":"$day"}, 
               "count":{"$sum":1}
         }},
-        { "$sort": {"_id":1}},  
+        { "$sort": {"_id":-1}},  
         { "$limit": 7 },
     ])
     // .exec((err, result) => {
