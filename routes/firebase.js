2ff1a485b2d84355c72c811cd2d9c6adc45d9c0a
andrea leo <andrea.leo83@gmail.com>
2018-10-19 12:47:54 +0200

    createtokenext

---
+++ routes/firebase.js
@@ -0,0 +1,99 @@
+
+'use strict';
+
+// Modules imports
+// const rp = require('request-promise');
+const express = require('express');
+// const bodyParser = require('body-parser');
+var router = express.Router();
+var User = require("../models/user");
+var firebaseService = require("../services/firebaseService");
+var config = require('../config/database'); 
+var jwt = require('jsonwebtoken');
+var validtoken = require('../middleware/valid-token');
+var secret = process.env.SECRET || config.secret;
+var requestUtil = require('../utils/requestUtil');
+
+
+router.post('/createtokenext', validtoken, function (req, res) {
+
+    var project_id = req.query.project_id;
+
+    console.log("project_id", project_id);
+
+    if (!project_id) {
+        console.error("project_id parameter is required");
+        res.status(400).send({ success: false, msg: "project_id parameter is required" });
+    }
+
+    try {
+        var token = requestUtil.getToken(req.headers);
+        console.log("token", token);
+        // verify a token symmetric - synchronous
+        var decoded = jwt.verify(token, secret);
+        console.log("decoded", decoded);
+   
+        // var email = decoded.email;
+        var extuid = decoded.external_id;
+        console.log("extuid", extuid);
+
+    } catch(err) {
+        console.error("myerror", err);
+        res.status(401).send({ success: false, msg: 'Authentication failed', err: err });
+    }
+
+  
+
+        var firebaseuid = extuid + '_' + extuid;
+        console.log("firebaseuid", firebaseuid);
+
+        firebaseService.createCustomToken(firebaseuid).then(customAuthToken => {
+        
+
+            return res.json({firebaseToken: customAuthToken});   
+        })
+        .catch(err => {
+            console.error("err", err);
+            const ret = {
+                error_message: 'Authentication error: Cannot verify access token.'
+            };
+                return res.status(403).send(ret);
+        });
+    
+  
+
+
+  });
+
+// router.post('/createtoken', [passport.authenticate(['basic', 'jwt'], { session: false }), validtoken], function (req, res) {
+//     console.log("req.user.id", req.user.id);
+//     User.findById(req.user.id, 'email firstname lastname  id', function (err, user) {
+//         console.log("err", err);
+//       if (err) throw err;
+  
+//       if (!user) {
+//         res.status(401).send({ success: false, msg: 'Authentication failed. User not found.' });
+//       } else {
+
+
+                  
+//                 var uid = user.id;
+//                 console.log("uid",uid);
+
+//                 firebaseService.createCustomToken(uid).then(customAuthToken => {
+                
+       
+//                     return res.status(200).send(customAuthToken);   
+//                 })
+//                 .catch(err => {
+//                     const ret = {
+//                         error_message: 'Authentication error: Cannot verify access token.'
+//                     };
+//                         return res.status(403).send(ret);
+//                 });
+          
+//       }
+//     });
+//   });
+  
+  module.exports = router;
