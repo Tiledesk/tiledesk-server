05bf2ac7b1ba2335bdfc81113b8bdb9205422c8b
andrea leo <andrea.leo83@gmail.com>
2019-02-12 10:24:15 +0100

    atlas db

--- config/database.js
+++ config/database.js
@@ -1,6 +1,8 @@
 module.exports = {
   'secret':'nodeauthsecret',
-  'database': 'mongodb://localhost/chat21',
-  'databasetest': 'mongodb://localhost/tiledesk-test'
+  // 'database': 'mongodb://localhost/chat21',
+  'database': 'mongodb://test:test21@cluster0-shard-00-00-zyndg.mongodb.net:27017,cluster0-shard-00-01-zyndg.mongodb.net:27017,cluster0-shard-00-02-zyndg.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true',
+  //'databasetest': 'mongodb://localhost/tiledesk-test'
+  'databasetest': 'mongodb://test:test21@cluster0-shard-00-00-zyndg.mongodb.net:27017,cluster0-shard-00-01-zyndg.mongodb.net:27017,cluster0-shard-00-02-zyndg.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true',
   
 };
