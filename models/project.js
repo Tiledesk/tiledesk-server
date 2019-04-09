ce902e3d23a3c00184deea4762c12e3d30ee3e48
andrea leo <andrea.leo83@gmail.com>
2019-04-09 09:30:28 +0200

    expired check

--- models/project.js
+++ models/project.js
@@ -1,5 +1,6 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
+var winston = require('../config/winston');
 
 var ProjectSchema = new Schema({
   _id: Schema.Types.ObjectId,
@@ -32,13 +33,38 @@ var ProjectSchema = new Schema({
     type: String,
     select: false
   },
+  profile: {
+    type: String,
+    default: 'free',
+    required: true
+  },
+  trialDays: {
+    type: Number,
+    default: 30
+  },
   createdBy: {
     type: String,
     required: true
   }
 },{
-  timestamps: true
+  timestamps: true,
+  toJSON: { virtuals: true } //used to polulate messages in toJSON// https://mongoosejs.com/docs/populate.html
 }
 );
+
+ProjectSchema.virtual('trialExpired').get(function () {
+  // https://stackoverflow.com/questions/6963311/add-days-to-a-date-object
+  let now = new Date();
+  winston.debug("now.getTime() " + now.getTime());
+  winston.debug("this.createdAt.getTime() " + this.createdAt.getTime());
+  winston.debug("trial " + this.trialDays *  86400000 );
+  if (this.createdAt.getTime() + this.trialDays *  86400000 > now.getTime()){
+    winston.debug("not expired");
+    return false;
+  }else {
+    winston.debug("expired");
+    return true;
+  }
+});
 
 module.exports = mongoose.model('project', ProjectSchema);
