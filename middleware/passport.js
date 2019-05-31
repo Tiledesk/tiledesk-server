4332cb126a72f8116fa9548bec3ad700b5e6d87e
andrea leo <andrea.leo83@gmail.com>
2019-05-31 12:12:23 +0200

    passport for bot

--- middleware/passport.js
+++ middleware/passport.js
@@ -42,18 +42,30 @@ module.exports = function(passport) {
               var decoded = jwt.decode(rawJwtToken);
               winston.info("decoded: ", decoded );
 
-              if (request.url=="uni" && request.project && request.project.jwtSecret) {
-                winston.info("uni config.jwtSecret: "+ config.jwtSecret );
-                done(null, request.project.jwtSecret);
-              } else {
+              // qui arriva questo 
+              // decoded:  {"_id":"5ce3ee855c520200176c189e","updatedAt":"2019-05-31T09:50:22.949Z","createdAt":"2019-05-21T12:26:45.192Z","name":"botext","url":"https://tiledesk-v2-simple--andrealeo83.repl.co","id_project":"5ce3d1ceb25ad30017274bc5","trashed":false,"createdBy":"5ce3d1c7b25ad30017274bc2","__v":0,"external":true,"iat":1559297130,"aud":"https://tiledesk.com","iss":"https://tiledesk.com","sub":"5ce3ee855c520200176c189e@tiledesk.com/bot"}
+
+              if (decoded && decoded.sub && decoded.sub.endsWith('/bot')) {
+                winston.info("bot: ", decoded.sub );
+                Faq_kb.findOne({_id: decoded._id}, function(err, faq_kb) {
+                  winston.info("faq_kb: ", faq_kb );
+                  winston.info("faq_kb.secret: ", faq_kb.secret );
+                  done(null, faq_kb.secret);
+                });
+              }
+              // if (request.url=="uni" && request.project && request.project.jwtSecret) {
+              //   winston.info("uni config.jwtSecret: "+ config.jwtSecret );
+              //   done(null, request.project.jwtSecret);
+              // } 
+              else {
                 winston.info("config.jwtSecret: "+ config.secret );
                 done(null, config.secret);
               }             
             }
        }
 
-   var secret23 = process.env.SECRET || config.secret;
-   winston.debug("secret23", secret23);
+  //  var secret23 = process.env.SECRET || config.secret;
+  //  winston.debug("secret23", secret23);
 
   winston.info("passport opts: ", opts);
 
