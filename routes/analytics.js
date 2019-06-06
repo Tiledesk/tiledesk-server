d3f167fa225b352379358a858e958a6e6abccecf
Andrea Leo <andrealeo83@users.noreply.github.com>
2019-06-06 10:27:54 +0200

    request waiting by day

--- routes/analytics.js
+++ routes/analytics.js
@@ -300,6 +300,41 @@ router.get('/requests/count', function(req, res) {
   });
 
 
+router.get('/requests/waiting/day', function(req, res) {
+  
+    winston.debug(req.params);
+    winston.debug("req.projectid",  req.projectid);    
+   
+      
+    AnalyticResult.aggregate([
+        { "$match": {"id_project":req.projectid }},
+        { "$project":{
+          "year":{"$year":"$createdAt"}, 
+          "month":{"$month":"$createdAt"},
+          "day":{"$day":"$createdAt"},
+          "waiting_time_project" : "$waiting_time"
+        }}, 
+        { "$group": { 
+          "_id": {"day":"$day","month":"$month", "year": "$year"}, 
+         "waiting_time_avg":{"$avg": "$waiting_time_project"}
+        }
+      },
+      
+    ])
+      .exec(function(err, result) {
+
+          if (err) {
+            winston.debug(err);
+            return res.status(500).send({success: false, msg: 'Error getting analytics.'});
+          }
+          winston.debug(result);
+
+          res.json(result);
+    });
+
+  });
+
+
   router.get('/requests/waiting/month', function(req, res) {
   
     winston.debug(req.params);
