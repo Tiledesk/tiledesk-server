aa8172f9fd3be8f81a1de7d08e228d7a23a6b34a
andrea leo <andrea.leo83@gmail.com>
2019-06-13 10:20:53 +0200

    check fix

--- utils/firebaseConnector.js
+++ utils/firebaseConnector.js
@@ -5,7 +5,7 @@ var winston = require('../config/winston');
 var chat21Enabled = process.env.CHAT21_ENABLED;
 winston.info("chat21Enabled: "+chat21Enabled);
 
-      if (chat21Enabled && chat21Enabled ===true){
+      if (chat21Enabled && chat21Enabled ==true){
 
       //firestore
       var firebaseConfig = require('../config/firebase');
