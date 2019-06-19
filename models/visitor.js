cfccf2bf0f190a601c42e211e164735212c2eb2b
andrea leo <andrea.leo83@gmail.com>
2019-06-19 19:32:32 +0200

    visitor added

---
+++ models/visitor.js
@@ -0,0 +1,38 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+/*
+Visitors are useful for representing logged-out users that didn't interact with the Tiledesk widget yet. 
+After specific actions Visitors can be converted to Leads in Tiledesk.
+The Visitors resource provides methods to fetch, update, convert and delete.
+*/
+
+var VisitorSchema = new Schema({    
+    name: {
+        type: String,
+    },
+    ip: {
+        type: String,
+    },
+    attributes: {
+        type: Object,
+    },
+    id_project: {
+        type: String,
+        required: true,
+        index: true
+    },   
+    createdBy: {
+        type: String,
+        required: true
+    }    
+    },{
+      timestamps: true
+    }
+);
+
+
+var VisitorModel = mongoose.model('visitor', VisitorSchema);
+
+
+module.exports = VisitorModel;
