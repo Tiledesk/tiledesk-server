95ef10cf955f912a1e7ac50799918eaddd16d4a6
andrea leo <andrea.leo83@gmail.com>
2019-07-15 11:35:21 +0200

    projectservice added createAndReturnProjectAndProjectUser

--- services/projectService.js
+++ services/projectService.js
@@ -8,66 +8,79 @@ var winston = require('../config/winston');
 
 class ProjectService {
 
-
-  create(name, createdBy, settings) {
-
+  createAndReturnProjectAndProjectUser(name, createdBy, settings) {
     return new Promise(function (resolve, reject) {
 
     
-        var newProject = new Project({
-            _id: new mongoose.Types.ObjectId(),
-            name: name,
-            // createdBy: req.body.id_user,
-            // updatedBy: req.body.id_user
-            activeOperatingHours: false,
-            //operatingHours: req.body.hours,
-            settings: settings,
+      var newProject = new Project({
+          _id: new mongoose.Types.ObjectId(),
+          name: name,
+          // createdBy: req.body.id_user,
+          // updatedBy: req.body.id_user
+          activeOperatingHours: false,
+          //operatingHours: req.body.hours,
+          settings: settings,
+          createdBy: createdBy,
+          updatedBy: createdBy
+        });
+        // console.log('NEW PROJECT ', newProject)
+      
+        return newProject.save(function (err, savedProject) {
+          if (err) {
+            winston.error('--- > ERROR ', err)
+            return reject({ success: false, msg: 'Error saving object.' });
+          }
+          // console.log('--- SAVE PROJECT ', savedProject)
+          //res.json(savedProject);
+      
+          // PROJECT-USER POST
+          var newProject_user = new Project_user({
+            // _id: new mongoose.Types.ObjectId(),
+            id_project: savedProject._id,
+            id_user: createdBy,
+            role: 'owner',
+            user_available: true,
             createdBy: createdBy,
             updatedBy: createdBy
           });
-          // console.log('NEW PROJECT ', newProject)
-        
-          return newProject.save(function (err, savedProject) {
+      
+         return  newProject_user.save(function (err, savedProject_user) {
             if (err) {
               winston.error('--- > ERROR ', err)
-              return reject({ success: false, msg: 'Error saving object.' });
+              return reject(err);
             }
-            // console.log('--- SAVE PROJECT ', savedProject)
-            //res.json(savedProject);
-        
-            // PROJECT-USER POST
-            var newProject_user = new Project_user({
-              // _id: new mongoose.Types.ObjectId(),
-              id_project: savedProject._id,
-              id_user: createdBy,
-              role: 'owner',
-              user_available: true,
-              createdBy: createdBy,
-              updatedBy: createdBy
+
+
+            return departmentService.createDefault(savedProject._id, createdBy).then(function(createdDepartment){
+              winston.info("Project created", savedProject.toObject() );
+              // console.info("Project user created", savedProject_user );
+              // console.info("Department created", createdDepartment );
+              return resolve({project:savedProject, project_user: savedProject_user});
             });
-        
-           return  newProject_user.save(function (err, savedProject_user) {
-              if (err) {
-                winston.error('--- > ERROR ', err)
-                return reject(err);
-              }
+          });
 
 
-              return departmentService.createDefault(savedProject._id, createdBy).then(function(createdDepartment){
-                winston.info("Project created", savedProject.toObject() );
-                // console.info("Project user created", savedProject_user );
-                // console.info("Department created", createdDepartment );
-                return resolve(savedProject);
-              });
-            });
+      });  
 
+  });
 
-        });  
+  }
 
+  create(name, createdBy, settings) {
+    var that = this;
+    return new Promise(function (resolve, reject) {
+      return that.createAndReturnProjectAndProjectUser(name, createdBy, settings).then(function(projectAndProjectUser){
+        return resolve(projectAndProjectUser.project);
+      }).catch(function(err){
+        return reject(err);
+      });
     });
   }
 
+
+
 }
+
 var projectService = new ProjectService();
 
 
