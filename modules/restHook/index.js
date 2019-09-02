564242501823d2f7bdf93694a8b0cf3a94b5b564
Glitch (tiledesk-server-dev2) <none>
2019-09-02 15:44:11 +0000

    f

--- modules/restHook/index.js
+++ modules/restHook/index.js
@@ -1,6 +1,6 @@
 console.log("10")
 const resthookRoute = require("./route/subscription");
 console.log("101")
-const subscriptionNotifier = require("./subscriptionNotifier");
+const subscriptionNotifier = require("./SubscriptionNotifier");
 console.log("102")
 module.exports = {subscriptionNotifier:subscriptionNotifier, resthookRoute:resthookRoute};
