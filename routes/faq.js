3e05fa83dc2a9da9e6d871b3e404bc96242fd6d4
andrea leo <andrea.leo83@gmail.com>
2019-09-09 10:46:09 +0200

    faq get list with full text

--- routes/faq.js
+++ routes/faq.js
@@ -187,21 +187,49 @@ router.get('/:faqid', function (req, res) {
 
 
 
-router.get('/', function (req, res) {
+router.get('/', function (req, res, next) {
   var query = {};
 
-  winston.debug("req.query", req.query);
+  console.log("GET ALL FAQ OF THE BOT ID (req.query): ", req.query);
 
   if (req.query.id_faq_kb) {
     query.id_faq_kb = req.query.id_faq_kb;
   }
 
-  winston.debug("query", query);
+  if (req.query.text) {
+    console.log("GET FAQ req.projectid", req.projectid);
 
-  Faq.find(query, function (err, faq) {
-    if (err) return next(err);
-    res.json(faq);
-  });
+    // query.$text = req.query.text;
+    query.$text = { "$search": req.query.text };
+    query.id_project = req.projectid
+  }
+
+  console.log("GET FAQ query", query);
+
+  // query.$text = {"$search": "question"};
+
+  return Faq.find(query).
+    populate('faq_kb').
+    exec(function (err, faq) {
+      console.log("GET FAQ ", faq);
+
+      if (err) {
+        console.log('GET FAQ err ', err)
+        return next(err)
+      };
+      console.log('GET FAQ  ', faq)
+      res.json(faq);
+
+    });
+
+  // Faq.find(query, function (err, faq) {
+  //   if (err) {
+  //     console.log('GET FAQ err ', err)
+  //     return next(err)
+  //   };
+  //   console.log('GET FAQ  ', faq)
+  //   res.json(faq);
+  // });
 
 });
 
