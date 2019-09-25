a99827cb51f882debbb34c4761d930081511c2e7
Andrea Leo <andrealeo83@users.noreply.github.com>
2019-09-25 11:49:49 +0200

    Update passport.js

--- middleware/passport.js
+++ middleware/passport.js
@@ -15,7 +15,7 @@ var Faq_kb = require("../models/faq_kb");
 var Project = require('../models/project');
 var Subscription = require('../models/subscription');
 var jwt = require('jsonwebtoken');
-const URL = require('url');
+const url = require('url');
 
 module.exports = function(passport) {
     
