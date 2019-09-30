761cd4281074d24b059fd1154b8fba7dd3030e23
Andrea Leo <andrealeo83@users.noreply.github.com>
2019-09-30 09:20:13 +0200

    Add files via upload

---
+++ channels/chat21/firebaseauth.js
@@ -0,0 +1,37 @@
+
+'use strict';
+
+// Modules imports
+// const rp = require('request-promise');
+const express = require('express');
+// const bodyParser = require('body-parser');
+var router = express.Router();
+var firebaseService = require("../../services/firebaseService");
+
+
+
+router.post('/createCustomToken', function (req, res) {
+  
+                  
+              
+        var uid = req.user.id;
+        console.log("uid",uid);
+
+        firebaseService.createCustomToken(uid).then(customAuthToken => {
+        
+            return res.status(200).send(customAuthToken);   
+        })
+        .catch(err => {
+            const ret = {
+                error_message: 'Authentication error: Cannot verify access token.'
+            };
+                return res.status(403).send(ret);
+        });
+                 
+  });
+ 
+  
+
+
+  module.exports = router;
+
