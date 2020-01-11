942b6fdfd50561c1ef7bb33c06647eafcc0be5f8
andrea leo <andrea.leo83@gmail.com>
2020-01-11 18:11:17 +0100

    user fix

--- routes/users.js
+++ routes/users.js
@@ -10,11 +10,31 @@ router.get('/', function (req, res, next) {
 });
 
 /* update user firstname/lastname */
+// deprecated
 router.put('/updateuser/:userid', function (req, res) {
 
   console.log('UPDATE USER - REQ BODY ', req.body);
 
   User.findByIdAndUpdate(req.params.userid, req.body, { new: true, upsert: true }, function (err, updatedUser) {
+    if (err) {
+      console.log(err);
+      return res.status(500).send({ success: false, msg: err });
+    }
+
+    console.log('UPDATED USER ', updatedUser);
+    if (!updatedUser) {
+      return res.status(404).send({ success: false, msg: 'User not found' });
+    }
+    res.json({ success: true, updatedUser });
+  });
+});
+
+
+router.put('/', function (req, res) {
+
+  console.log('UPDATE USER - REQ BODY ', req.body);
+
+  User.findByIdAndUpdate(req.user._id, req.body, { new: true, upsert: true }, function (err, updatedUser) {
     if (err) {
       console.log(err);
       return res.status(500).send({ success: false, msg: err });
