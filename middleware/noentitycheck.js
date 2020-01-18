ba998df4c30992e49b4a9c15a423a27ec9374b4f
andrea leo <andrea.leo83@gmail.com>
2020-01-18 11:18:07 +0100

    log fix

--- middleware/noentitycheck.js
+++ middleware/noentitycheck.js
@@ -4,7 +4,7 @@ var winston = require('../config/winston');
 
 module.exports = 
     function(req,res,next){ 
-        winston.debug("before isablePassportEntityCheck=true");
+        winston.debug("before disablePassportEntityCheck=true");
         req.disablePassportEntityCheck = true;
         winston.debug("disablePassportEntityCheck=true"); 
         next();
