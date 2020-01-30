608eb954ed32ab4c4a6b494e9ffe2f7a59952943
andrea leo <andrea.leo83@gmail.com>
2020-01-30 10:16:15 +0100

    message complete without request.mesages

--- event/messageEvent.js
+++ event/messageEvent.js
@@ -65,6 +65,82 @@ function populateMessage(message, eventPrefix) {
       var requestJson = request.toJSON();
      
 
+      // Message.find({recipient:  request.request_id, id_project: request.id_project}).sort({updatedAt: 'asc'}).exec(function(err, messages) {                        
+      //   winston.debug("populateMessage messages",messages );
+
+
+        // requestJson.messages = messages;
+
+
+            if (request.department && request.department.id_bot) {
+              // if (request.department) {
+              Faq_kb.findById(request.department.id_bot, function(err, bot) {
+                winston.debug('bot', bot);
+                
+
+                requestJson.department.bot = bot
+                
+                messageJson.request = requestJson;
+                // winston.debug('messageJson', messageJson);
+                winston.debug("Subscription.emit",messageJson );
+                messageEvent.emit(eventPrefix,messageJson );
+
+                // if (messages && messages.length==1){
+                if (message.text === request.first_text){
+                  messageEvent.emit(eventPrefix+'.first', messageJson );
+                }
+
+              });
+
+              
+            }else {
+              messageJson.request = requestJson;
+              winston.debug("Subscription.emit",messageJson );
+              messageEvent.emit(eventPrefix, messageJson );   
+              
+              // if (messages && messages.length==1){
+              if (message.text === request.first_text){
+                messageEvent.emit(eventPrefix+'.first', messageJson );
+              }
+
+            }
+
+      // });
+
+  }
+   
+  });
+
+}
+
+function populateMessageWithRequestMessages(message, eventPrefix) {
+
+
+  winston.debug("Subscription.notify "+eventPrefix, message.toObject());
+  
+  Request.findOne({request_id:  message.recipient, id_project: message.id_project}).
+  populate('lead').
+  populate('department').        
+  populate({path:'requester',populate:{path:'id_user'}}).
+  exec(function (err, request) {
+
+    if (err) {
+      winston.error("Error getting request on messageEvent.populateMessage",err );
+      return messageEvent.emit(eventPrefix, message);
+    }
+
+    if (request) {
+
+      // if (request.messages_count==0){
+      //   messageEvent.emit('message.create.first', messageJson );
+      // }
+
+
+      var messageJson = message.toJSON();
+      
+      var requestJson = request.toJSON();
+     
+
       Message.find({recipient:  request.request_id, id_project: request.id_project}).sort({updatedAt: 'asc'}).exec(function(err, messages) {                        
         winston.debug("populateMessage messages",messages );
 
