e80876fe91e6ad2d41de894f2ce4cae721835cbf
andrea leo <andrea.leo83@gmail.com>
2020-04-04 14:38:01 +0200

    log

--- channels/chat21/firebaseConnector.js
+++ channels/chat21/firebaseConnector.js
@@ -9,22 +9,22 @@ if (chat21Enabled && chat21Enabled == "true"){
   // if (chat21Enabled){
 
       //firestore
-      winston.info("chat21 channel enabled"); 
+      winston.info("Chat21 channel enabled"); 
       var firebaseConfig = require('./firebaseConfig');
 
       var firebaseConfigFilePath = process.env.FIREBASE_CONFIG_FILE || '../../.firebasekey.json';
 
-      winston.info('firebaseConnector firebaseConfig.databaseURL:'+ firebaseConfig.databaseUrl);
+      winston.info('FirebaseConnector firebaseConfig.databaseURL:'+ firebaseConfig.databaseUrl);
 
       var private_key = process.env.FIREBASE_PRIVATE_KEY;
-      winston.info('firebaseConnector private_key:'+ private_key);
+      winston.info('FirebaseConnector private_key:'+ private_key);
       // var private_key_masked = private_key.replace(/\d(?=\d{4})/g, "*");
       // winston.info('firebaseConnector private_key:'+ private_key_masked);// <-- TODO obscure it
       
       var client_email = process.env.FIREBASE_CLIENT_EMAIL;
-      winston.info('firebaseConnector client_email:'+ client_email);
+      winston.info('FirebaseConnector client_email:'+ client_email);
       var firebase_project_id = process.env.FIREBASE_PROJECT_ID;
-      winston.info('firebaseConnector firebase_project_id:'+ firebase_project_id);
+      winston.info('FirebaseConnector firebase_project_id:'+ firebase_project_id);
 
       // https://stackoverflow.com/questions/41287108/deploying-firebase-app-with-service-account-to-heroku-environment-variables-wit
       var serviceAccount;
