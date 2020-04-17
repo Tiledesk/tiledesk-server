1bd1a20f18ca8c5878558762c73388caab66d10d
andrea leo <andrea.leo83@gmail.com>
2020-04-17 16:28:53 +0200

    log

--- pubmodules/messageTransformer/messageTransformerInterceptor.js
+++ pubmodules/messageTransformer/messageTransformerInterceptor.js
@@ -15,14 +15,14 @@ class MessageTransformerInterceptor {
         winston.info("MessageTransformerInterceptor listener start ");
         
         messagePromiseEvent.on('message.create.simple.before', async (data) => {
-            winston.info('MessageTransformerInterceptor message.create.simple.before', data); 
+            winston.debug('MessageTransformerInterceptor message.create.simple.before', data); 
 
             var message = data.beforeMessage;
             
             if (!message.text) { //for image i think
                 return data;
             }
-            
+
             // https://stackoverflow.com/questions/413071/regex-to-get-string-between-curly-braces
             var re = /\${([^}]+)\}/;
             var m = message.text.match(re);
@@ -40,7 +40,7 @@ class MessageTransformerInterceptor {
 
             // if (message.text.indexOf("${")>-1) {
             
-                winston.info('MessageTransformerInterceptor enter');
+                winston.debug('MessageTransformerInterceptor enter');
                 var label = await labelService.getByLanguageAndKey(message.id_project,language, messageExtracted);
                 if (label) {
                     message.text=label;  
@@ -54,7 +54,7 @@ class MessageTransformerInterceptor {
                 // })
                 // winston.info('MessageTransformerInterceptor enter here???');
             } else {
-                winston.info('MessageTransformerInterceptor return');
+                winston.debug('MessageTransformerInterceptor return');
                 return data;
             }
            
