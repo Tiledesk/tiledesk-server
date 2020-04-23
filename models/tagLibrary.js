4e1bc23dc0975863011e18ec0a0fcca4ecf6235f
andrea leo <andrea.leo83@gmail.com>
2020-04-23 12:55:31 +0200

    tag fix

--- models/tagLibrary.js
+++ models/tagLibrary.js
@@ -1,24 +1,8 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 var winston = require('../config/winston');
+var TagSchema = require('./tag');
 
-var TagSchema = new Schema({
-  
-  tag: {
-    type: String,
-    required: true,
-    index: true
-  },  
-  color: {
-    type: String,
-    required: false,
-    index: false
-  },  
-  attributes: {
-    type: Object,
-  },
-}
-);
 
 
 var TagLibrarySchema = new Schema({
