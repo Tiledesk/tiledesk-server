af0919e9469e7557f7c2cd04bc01a7269aefb25f
andrea leo <andrea.leo83@gmail.com>
2020-06-29 17:41:17 +0200

    deploy script

--- deploy-beta.sh
+++ deploy-beta.sh
@@ -3,8 +3,9 @@ version=`node -e 'console.log(require("./package.json").version)'`
 echo "version $version"
 
 if [ "$version" != "" ]; then
-    git tag -a "v$version" -m "`git log -1 --format=%s`"
+    git tag -a "$version" -m "`git log -1 --format=%s`"
     echo "Created a new tag, v$version"
     git push --tags
     npm publish --access public
 fi
+git push
