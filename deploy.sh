2d9c96a93e5c96a39db70f182b714d5eb4d534a0
andrea leo <andrea.leo83@gmail.com>
2020-07-03 19:48:03 +0200

    v fix

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,7 @@ echo "version $version"
 
 if [ "$version" != "" ]; then
     git tag -a "$version" -m "`git log -1 --format=%s`"
-    echo "Created a new tag, v$version"
+    echo "Created a new tag, $version"
     git push --tags
     npm publish --access public
 fi
