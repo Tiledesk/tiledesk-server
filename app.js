571ff9fc60fc56230a095b630c62f24e078029d7
andrea leo <andrea.leo83@gmail.com>
2020-07-15 10:18:16 +0200

    cors

--- app.js
+++ app.js
@@ -139,12 +139,16 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*"); //qui dice cequens attento
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization, X-XSRF-Token");
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
-  next();
-});
+
+
+// app.use(function (req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "*"); //qui dice cequens attento
+//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization, X-XSRF-Token");
+//   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
+//   next();
+// });
+
+
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
