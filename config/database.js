3a661bea936f4cf640d5fd49d59710d309099d2a
andrea leo <andrea.leo83@gmail.com>
2020-09-03 10:27:44 +0200

    fix

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 module.exports = {
   'secret':'nodeauthsecret',
-  'database': 'mongodb://tiledesk-user1:CGjsYc06aD4YT4SA@tiledesk-prod-shard-00-00-vmvst.mongodb.net:27017,tiledesk-prod-shard-00-01-vmvst.mongodb.net:27017,tiledesk-prod-shard-00-02-vmvst.mongodb.net:27017/tiledesk-pre?ssl=true&replicaSet=tiledesk-prod-shard-0&authSource=admin&retryWrites=true',
+  'database': 'mongodb://localhost:27017/tiledesk',
   'databaselogs': 'mongodb://localhost:27017/tiledesk-logs',
   'databasetest': 'mongodb://localhost:27017/tiledesk-test'
 };
