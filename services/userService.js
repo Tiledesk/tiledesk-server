f173a5033b9f93ba5abb1f5fdad3691322559cb4
andrea leo <andrea.leo83@gmail.com>
2020-10-14 11:29:56 +0200

    #TD-251 email lowercase fix

--- services/userService.js
+++ services/userService.js
@@ -4,42 +4,29 @@ var mongoose = require('mongoose');
 var winston = require('../config/winston');
 
 class UserService {
-//TODO providerId cambia tutte classi di test perche è stato aggiunto providerId,
-// add authType
-    signup ( email, password, firstname, lastname, emailverified
-        // , authType
-        ) {
+
+    signup ( email, password, firstname, lastname, emailverified) {
         return new Promise(function (resolve, reject) {
-
-            // var auth = new Auth({
-            //     password: password
-            // });
-            // auth.save(function (err, authSaved) {
-            //     if (err) {
-            //         return reject(err);
-            //     }
                 
-                var newUser = new User({
-                    _id: new mongoose.Types.ObjectId(),
-                // providerId: providerId,
-                    email: email,
-                    password: password,
-                    firstname: firstname,
-                    lastname: lastname,
-                    emailverified: emailverified,
-                    // authType: authType
-                    // auth: authSaved._id
-                });
-                // save the user
-                newUser.save(function (err, savedUser) {
-                    if (err) {
-                        return reject(err);
-                    }
+            email = email.toLowerCase();
 
-                    winston.info('User created', savedUser.toObject());
-                    return resolve(savedUser);
-                });
-        //   });
+            var newUser = new User({
+                _id: new mongoose.Types.ObjectId(),
+                email: email,
+                password: password,
+                firstname: firstname,
+                lastname: lastname,
+                emailverified: emailverified,             
+            });
+            // save the user
+            newUser.save(function (err, savedUser) {
+                if (err) {
+                    return reject(err);
+                }
+
+                winston.info('User created', savedUser.toObject());
+                return resolve(savedUser);
+            });
         });
     }
 
