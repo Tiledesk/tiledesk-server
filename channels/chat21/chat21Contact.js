cce6baf6d8917c1d7aafd0302e2bb12e9470cd29
andrea leo <andrea.leo83@gmail.com>
2020-11-05 09:47:50 +0100

    chat21 contact detail service

--- channels/chat21/chat21Contact.js
+++ channels/chat21/chat21Contact.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var mongoose = require('mongoose');
 
 var Project_user = require("../../models/project_user");
 
@@ -14,43 +15,43 @@ var cacheUtil = require('../../utils/cacheUtil');
 const { Query } = require('mongoose');
 
 
-router.get('/', [passport.authenticate(['basic', 'jwt'], { session: false }), validtoken], async (req, res) => {
+router.get('/:contact_id', [passport.authenticate(['basic', 'jwt'], { session: false }), validtoken], async (req, res) => {
   winston.debug('REQ USER ID ', req.user._id);
 
-  var direction = -1; //-1 descending , 1 ascending
-  if (req.query.direction) {
-    direction = req.query.direction;
-  } 
-  winston.debug("direction",direction);
-
-  var sortField = "updatedAt";
-  if (req.query.sort) {
-    sortField = req.query.sort;
-  } 
-  winston.debug("sortField",sortField);
-
-  var sortQuery={};
-  sortQuery[sortField] = direction;
-
+  var contact_id = req.params.contact_id;
+  winston.debug('contact_id: '+ contact_id);
+  
 
   var projects = await Project_user.find({ id_user: req.user._id , role: { $in : [RoleConstants.OWNER, RoleConstants.ADMIN, RoleConstants.AGENT]}, status: "active" }).    
     exec(); 
 
-    var projectsArray = [];
-    projects.forEach(project => {
-      projectsArray.push(project.id_project);
-    });
+  var projectsArray = [];
+  projects.forEach(project => {
+    projectsArray.push(project.id_project);
+  });
     
-    var query = { id_project: { $in : projectsArray }, role: { $in : [RoleConstants.OWNER, RoleConstants.ADMIN, RoleConstants.AGENT]}, status: "active" };
-    winston.debug("query: ", query);
 
-    var teammates = await Project_user.find(query).
-    populate('id_project').
-    populate('id_user').
-    sort(sortQuery).
-    exec(); 
+  var isObjectId = mongoose.Types.ObjectId.isValid(contact_id);
+  winston.debug("isObjectId:"+ isObjectId);                             
 
-    var result = [];
+  var query = { id_project: { $in : projectsArray }, role: { $in : [RoleConstants.OWNER, RoleConstants.ADMIN, RoleConstants.AGENT]}, status: "active" };
+  winston.debug(' query: ',query);
+
+  if (isObjectId) {
+    query.id_user = contact_id;
+  }else {
+    query.uuid_user = contact_id;
+  }
+
+  winston.debug("query: ", query);
+
+  var teammates = await Project_user.findOne(query).
+  populate('id_project').
+  populate('id_user').
+  exec(); 
+
+  var result = [];
+  if (teammates && teammates.length>0) {  
     teammates.forEach(teammate => {
       
       var contact = {};
@@ -92,6 +93,98 @@ router.get('/', [passport.authenticate(['basic', 'jwt'], { session: false }), va
       
 
     });
+  }
+  
+  winston.debug("send");
+  res.json(result);
+    
+  
+});
+
+
+
+
+router.get('/', [passport.authenticate(['basic', 'jwt'], { session: false }), validtoken], async (req, res) => {
+  winston.debug('REQ USER ID ', req.user._id);
+
+  var direction = -1; //-1 descending , 1 ascending
+  if (req.query.direction) {
+    direction = req.query.direction;
+  } 
+  winston.debug("direction",direction);
+
+  var sortField = "updatedAt";
+  if (req.query.sort) {
+    sortField = req.query.sort;
+  } 
+  winston.debug("sortField",sortField);
+
+  var sortQuery={};
+  sortQuery[sortField] = direction;
+
+
+  var projects = await Project_user.find({ id_user: req.user._id , role: { $in : [RoleConstants.OWNER, RoleConstants.ADMIN, RoleConstants.AGENT]}, status: "active" }).    
+    exec(); 
+
+    var projectsArray = [];
+    projects.forEach(project => {
+      projectsArray.push(project.id_project);
+    });
+    
+    var query = { id_project: { $in : projectsArray }, role: { $in : [RoleConstants.OWNER, RoleConstants.ADMIN, RoleConstants.AGENT]}, status: "active" };
+    winston.debug("query: ", query);
+
+    var teammates = await Project_user.find(query).
+    populate('id_project').
+    populate('id_user').
+    sort(sortQuery).
+    exec(); 
+
+    var result = [];
+
+    if (teammates && teammates.length>0) {  
+      teammates.forEach(teammate => {
+        
+        var contact = {};
+        if (teammate.id_user) {
+          contact.uid = teammate.id_user._id;
+          contact.email = teammate.id_user.email;
+          contact.firstname = teammate.id_user.firstname;
+          contact.lastname = teammate.id_user.lastname;
+
+          if (teammate.id_project) {
+            contact.description =  teammate.id_project.name;
+          }
+
+          // if (teammate.id_user.createdAt) {
+          //   contact.timestamp = teammate.id_user.createdAt.getTime();
+          // }
+          
+          // winston.info("teammate: "+ JSON.stringify(teammate));
+
+          var contactFound = result.filter(c => c.uid === contact.uid );
+          winston.debug("contactFound: "+ JSON.stringify(contactFound));
+
+          // var index = result.indexOf(contactFound);
+          let index = result.findIndex(c => c.uid === contact.uid );
+
+          winston.debug("index: "+ index);
+
+          if (contactFound.length==0) {
+            winston.debug("not found");
+            result.push(contact);
+          }else {
+            winston.debug("found",contactFound);
+            // contactFound[0].description = "sssss";
+            contactFound[0].description= contactFound[0].description + ", "+teammate.id_project.name;
+            result[index] = contactFound[0];
+            
+          }
+        }
+        
+
+      });
+    }
     winston.debug("send");
     res.json(result);
     
