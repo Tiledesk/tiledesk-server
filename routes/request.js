5e143c5353121521ff171099601e1a64992432ee
andrea leo <andrea.leo83@gmail.com>
2021-02-22 09:32:38 +0100

    log

--- routes/request.js
+++ routes/request.js
@@ -303,7 +303,6 @@ router.delete('/:requestid/participants/:participantid', function (req, res) {
 
 
 
-
 // TODO make a synchronous chat21 version (with query parameter?) with request.support_group.created
 router.put('/:requestid/departments', function (req, res) {
   winston.debug(req.body);
@@ -712,6 +711,7 @@ router.get('/', function (req, res, next) {
 
 
     if (!req.query.no_polutate) {
+      winston.info('REQUEST ROUTE - no_polutate truew ');
       q1.populate('department').
       populate('participatingBots').
       populate('participatingAgents').
