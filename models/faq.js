3347ccffcf90506f33c77f49b7f92a8203513741
andrea leo <andrea.leo83@gmail.com>
2021-06-15 15:31:36 +0200

    Removed answer field from the fulltext search of the faqs

--- models/faq.js
+++ models/faq.js
@@ -93,8 +93,14 @@ FaqSchema.index({ id_project: 1, id_faq_kb: 1, question: 1 });
 // https://docs.mongodb.com/manual/core/index-text/
 // https://docs.mongodb.com/manual/tutorial/specify-language-for-text-index/
 // https://docs.mongodb.com/manual/reference/text-search-languages/#text-search-languages
-FaqSchema.index({question: 'text', answer: 'text'},
- {"name":"faq_fulltext","default_language": defaultFullTextLanguage,"language_override": "language", weights: {question: 10,answer: 1}}); // schema level
+
+
+FaqSchema.index({question: 'text'},
+ {"name":"faq_fulltext","default_language": defaultFullTextLanguage,"language_override": "language"}); // schema level
+
+//  FaqSchema.index({question: 'text', answer: 'text'},
+//  {"name":"faq_fulltext","default_language": defaultFullTextLanguage,"language_override": "language", weights: {question: 10,answer: 1}}); // schema level
+
 
 FaqSchema.index({ id_project: 1, id_faq_kb: 1, intent_display_name: 1  }, { unique: true }); 
 
