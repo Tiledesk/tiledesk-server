bda402913036201a1bfe33fd26168d879ba10acd
Andrea Leo <andrea.leo83@gmail.com>
2021-07-15 09:48:08 +0200

    MessageRoot endpoint also for group messages

--- routes/messagesRoot.js
+++ routes/messagesRoot.js
@@ -7,8 +7,6 @@ var MessageConstants = require("../models/messageConstants");
 var messageService = require("../services/messageService");
 var winston = require('../config/winston');
 
-// curl -v -X POST -H 'Content-Type:application/json' -u andrea.leo@frontiere21.it:258456td -d '{"senderFullname":"Bot","recipient":"USERID", "text":"ciao"}' https://tiledesk-server-pre.herokuapp.com/5f86c201189063003453a045/messages/
-// curl -v -X POST -H 'Content-Type:application/json' -u andrea.leo@f21.it:123456 -d '{"senderFullname":"Bot","recipient":"USERID", "text":"ciao"}' http://localhost:3000/609e3fd8e049553972114b88/messages/
 
 router.post('/', 
   async (req, res)  => {
@@ -17,10 +15,23 @@ router.post('/',
   winston.debug('req.params: ', req.params);
   winston.debug('req.params.request_id: ' + req.params.request_id);
 
-  let messageStatus = req.body.status || MessageConstants.CHAT_MESSAGE_STATUS.SENDING;
 
-  return messageService.create(req.body.sender || req.user._id, req.body.senderFullname || req.user.fullName, req.body.recipient, req.body.text,
-    req.projectid, req.user._id, messageStatus, req.body.attributes, req.body.type, req.body.metadata,  req.body.language, MessageConstants.CHANNEL_TYPE.DIRECT, req.body.channel).then(function(savedMessage){                    
+  let message = {
+    sender: req.body.sender || req.user._id, 
+    senderFullname: req.body.senderFullname || req.user.fullName, 
+    recipient: req.body.recipient, 
+    text: req.body.text, 
+    id_project: req.projectid, 
+    createdBy: req.user._id, 
+    status:  MessageConstants.CHAT_MESSAGE_STATUS.SENDING, 
+    attributes: req.body.attributes, 
+    type: req.body.type, 
+    metadata: req.body.metadata, 
+    language: req.body.language, 
+    channel_type: req.body.channel_type || MessageConstants.CHANNEL_TYPE.DIRECT, 
+    channel: req.body.channel
+};
+  return messageService.save(message).then(function(savedMessage){                    
       res.json(savedMessage);
     });
 
