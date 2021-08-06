af61298f0f5b529b27080cbb3152b83bc7da8eb7
Andrea Leo <andrea.leo83@gmail.com>
2021-08-06 16:39:18 +0200

    Campaigns direct and for group

--- routes/campaigns.js
+++ routes/campaigns.js
@@ -1,9 +1,7 @@
 var express = require('express');
 var router = express.Router();
-var Lead = require("../models/lead");
-var LeadConstants = require("../models/leadConstants");
+var Group = require("../models/group");
 var winston = require('../config/winston');
-var leadService = require("../services/leadService");
 var requestService = require("../services/requestService");
 var messageService = require("../services/messageService");
 var MessageConstants = require("../models/messageConstants");
@@ -11,13 +9,6 @@ const uuidv4 = require('uuid/v4');
 
 
 
-
-// curl -v -X POST -H 'Content-Type:application/json' -u andrea.leo@f21.it:123456 -d '{"text":"ciao"}' http://localhost:3000/5f897142c9e7ad9602a744c9/campaigns/
-// curl -v -X POST -H 'Content-Type:application/json' -u andrea.leo@f21.it:123456 -d '{"text":"ciao", "leadid":"213213221"}' http://localhost:3000/5f897142c9e7ad9602a744c9/campaigns/
-
-
-// curl -v -X POST -H 'Content-Type:application/json' -u andrea.leo@frontiere21.it:258456 -d '{"text":"ciao", "leadid":"5f8972c82db41c003473cb03"}' https://tiledesk-server-pre.herokuapp.com/5f86c201189063003453a045/campaigns/
-// curl -v -X POST -H 'Content-Type:application/json' -u andrea.leo@frontiere21.it:258456 -d '{"text":"ciao", "leadid":"5f8972c82db41c003473cb03", "request_id":"group-123456789112233"}' https://tiledesk-server-pre.herokuapp.com/5f86c201189063003453a045/campaigns/
 
 
 // this endpoint supports support-group- or groups. this create a conversation for the sender (agent console)
@@ -27,41 +18,112 @@ router.post('/', function (req, res) {
 
   winston.debug(req.body);
   winston.debug("req.user", req.user);
-  var request_id = req.body.request_id || 'support-group-'+uuidv4();
-  
+  var request_id = req.body.request_id || 'support-group-' + req.projectid + "-" + uuidv4();
+
+  // TODO cicla su segment
+
+
   // createWithIdAndRequester(request_id, project_user_id, lead_id, id_project, first_text, departmentid, sourcePage, language, userAgent, status, createdBy, attributes, subject, preflight, channel) {
-  return requestService.createWithIdAndRequester(request_id, req.projectuser._id, req.body.leadid, req.projectid, 
-    req.body.text, req.body.departmentid, req.body.sourcePage, 
+
+  //TODO USE NEW requestService.create()
+  return requestService.createWithIdAndRequester(request_id, req.projectuser._id, req.body.leadid, req.projectid,
+    req.body.text, req.body.departmentid, req.body.sourcePage,
     req.body.language, req.body.userAgent, null, req.user._id, req.body.attributes, req.body.subject, true, req.body.channel).then(function (savedRequest) {
 
       winston.info("savedRequest", savedRequest);
 
       // create(sender, senderFullname, recipient, text, id_project, createdBy, status, attributes, type, metadata, language, channel_type) {
       return messageService.create(req.body.sender || req.user._id, req.body.senderFullname || req.user.fullName, savedRequest.request_id, req.body.text,
-        req.projectid, req.user._id, messageStatus, req.body.attributes, req.body.type, req.body.metadata,  req.body.language, MessageConstants.CHANNEL_TYPE.DIRECT, req.body.channel).then(function(savedMessage){                    
+        req.projectid, req.user._id, messageStatus, req.body.attributes, req.body.type, req.body.metadata, req.body.language, MessageConstants.CHANNEL_TYPE.DIRECT, req.body.channel).then(function (savedMessage) {
           res.json(savedMessage);
-       });
+        });
 
-  });
+    });
 });
 
-// curl -v -X POST -H 'Content-Type:application/json' -u andrea.leo@frontiere21.it:258456 -d '{"text":"ciao", "recipient":"5f8972c82db41c003473cb03"}' https://tiledesk-server-pre.herokuapp.com/5f86c201189063003453a045/campaigns/direct
-router.post('/direct', function (req, res) {
+router.post('/direct', async function (req, res) {
 
   let messageStatus = req.body.status || MessageConstants.CHAT_MESSAGE_STATUS.SENDING;
 
   winston.debug(req.body);
   winston.debug("req.user", req.user);
-  
 
-  // create(sender, senderFullname, recipient, text, id_project, createdBy, status, attributes, type, metadata, language, channel_type) {
-  return messageService.create(req.body.sender || req.user._id, req.body.senderFullname || req.user.fullName, req.body.recipient, req.body.text,
-    req.projectid, req.user._id, messageStatus, req.body.attributes, req.body.type, req.body.metadata,  req.body.language, MessageConstants.CHANNEL_TYPE.DIRECT, req.body.channel).then(function(savedMessage){                    
-      res.json(savedMessage);
-    });
+  var recipients = [];
 
-  
+  var recipient = req.body.recipient;
+  if (recipient) {
+    recipients.push(recipient);
+  }
+
+  // TODO cicla su segment
+  var segment_id = req.body.segment_id;
+  if (segment_id) {
+
+  }
+
+  var group_id = req.body.group_id;
+  if (group_id) {
+    var group = await Group.findOne({ _id: group_id, id_project: req.projectid }).exec();
+    winston.info("group", group);
+
+    var recipients = group.members;
+    // winston.info("members", members);
+
+
+  }
+
+  winston.info("recipients", recipients);
+  winston.info("recipients.length: " + recipients.length);
+
+  if (recipients.length == 0) {
+    // return res
+  }
+
+  if (recipients.length == 1) {
+    return messageService.create(req.body.sender || req.user._id, req.body.senderFullname || req.user.fullName, req.body.recipient, req.body.text,
+      req.projectid, req.user._id, messageStatus, req.body.attributes, req.body.type, req.body.metadata, req.body.language, MessageConstants.CHANNEL_TYPE.DIRECT, req.body.channel).then(function (savedMessage) {
+
+        if (req.body.returnobject) {
+          return res.json(savedMessage);
+        } else {
+          return res.json({ success: true });
+        }
+
+      });
+  }
+
+
+  var promises = [];
+  recipients.forEach(recipient => {
+
+    // create(sender, senderFullname, recipient, text, id_project, createdBy, status, attributes, type, metadata, language, channel_type) {
+    var promise = messageService.create(req.body.sender || req.user._id, req.body.senderFullname || req.user.fullName, recipient, req.body.text,
+      req.projectid, req.user._id, messageStatus, req.body.attributes, req.body.type, req.body.metadata, req.body.language, MessageConstants.CHANNEL_TYPE.DIRECT, req.body.channel);
+    promises.push(promise);
+    // .then(function(savedMessage){                    
+    // result.push(savedMessage);
+    // res.json(savedMessage);
+  });
+
+  Promise.all(promises).then(function (data) {
+    if (req.body.returnobject) {
+      return res.json(data);
+    }
+  });
+
+  if (!req.body.returnobject) {
+    return res.json({ success: true });
+  }
+
+
+
+
 });
+
+
+
+
+
 
 
 
---
+++ test/campaignsRoute.js
@@ -0,0 +1,218 @@
+//During the test the env variable is set to test
+process.env.NODE_ENV = 'test';
+
+var User = require('../models/user');
+var projectService = require('../services/projectService');
+var Group = require('../models/group');
+var userService = require('../services/userService');
+
+//Require the dev-dependencies
+let chai = require('chai');
+let chaiHttp = require('chai-http');
+let server = require('../app');
+let should = chai.should();
+var winston = require('../config/winston');
+var jwt = require('jsonwebtoken');
+// chai.config.includeStack = true;
+
+var expect = chai.expect;
+var assert = chai.assert;
+
+chai.use(chaiHttp);
+
+describe('CampaignsRoute', () => {
+
+
+  // mocha test/campaignsRoute.js  --grep 'directSimpleNoOut'
+
+  it('directSimpleNoOut', function (done) {
+
+    var email = "test-message-create-" + Date.now() + "@email.com";
+    var pwd = "pwd";
+
+    userService.signup(email, pwd, "Test Firstname", "Test lastname").then(function (savedUser) {
+      projectService.createAndReturnProjectAndProjectUser("directSimple", savedUser._id).then(function (savedProjectAndPU) {
+
+        var savedProject = savedProjectAndPU.project;
+        var recipient = "5f8972c82db41c003473cb03";
+
+        chai.request(server)
+          .post('/' + savedProject._id + '/campaigns/direct')
+          .auth(email, pwd)
+          .set('content-type', 'application/json')
+          .send({ "text": "ciao", "recipient": recipient})
+          .end(function (err, res) {
+            //console.log("res",  res);
+            console.log("res.body", res.body);
+            res.should.have.status(200);
+            res.body.should.be.a('object');
+           
+                        
+            
+            expect(res.body.success).to.equal(true);
+
+
+            done();
+          });
+      });
+    });
+  });
+
+
+
+
+
+
+  // mocha test/campaignsRoute.js  --grep 'directSimple'
+
+  it('directSimple', function (done) {
+
+    var email = "test-message-create-" + Date.now() + "@email.com";
+    var pwd = "pwd";
+
+    userService.signup(email, pwd, "Test Firstname", "Test lastname").then(function (savedUser) {
+      projectService.createAndReturnProjectAndProjectUser("directSimple", savedUser._id).then(function (savedProjectAndPU) {
+
+        var savedProject = savedProjectAndPU.project;
+        var recipient = "5f8972c82db41c003473cb03";
+
+        chai.request(server)
+          .post('/' + savedProject._id + '/campaigns/direct')
+          .auth(email, pwd)
+          .set('content-type', 'application/json')
+          .send({ "text": "ciao", "recipient": recipient, returnobject: true })
+          .end(function (err, res) {
+            //console.log("res",  res);
+            console.log("res.body", res.body);
+            res.should.have.status(200);
+            res.body.should.be.a('object');
+
+            expect(res.body.channel_type).to.equal("direct");
+            expect(res.body.senderFullname).to.equal("Test Firstname Test lastname");
+            expect(res.body.sender).to.equal(savedUser._id.toString());
+            expect(res.body.recipient).to.equal(recipient);
+                        
+            
+            // expect(res.body.success).to.equal(true);
+
+
+            done();
+          });
+      });
+    });
+  });
+
+
+
+
+  // mocha test/campaignsRoute.js  --grep 'directGroupIdNoOut'
+
+  it('directGroupIdNoOut', function (done) {
+
+    var email = "test-message-create-" + Date.now() + "@email.com";
+    var pwd = "pwd";
+
+    userService.signup(email, pwd, "Test Firstname", "Test lastname").then(function (savedUser) {
+      projectService.createAndReturnProjectAndProjectUser("directSimple", savedUser._id).then(function (savedProjectAndPU) {
+
+        var savedProject = savedProjectAndPU.project;
+        
+        var userid = savedUser._id;
+
+        var newGroup = new Group({
+          name: "group1",
+          members: ["userid1", "userid2"],
+          // members: [userid, "userid2"],
+          trashed: false,
+          id_project: savedProject._id,
+          createdBy: userid,
+          updatedBy: userid
+        });
+        newGroup.save(function (err, savedGroup) {
+          console.log("savedGroup", savedGroup)
+
+
+          chai.request(server)
+            .post('/' + savedProject._id + '/campaigns/direct')
+            .auth(email, pwd)
+            .set('content-type', 'application/json')
+            .send({ "text": "ciao", "group_id": savedGroup._id.toString()})
+            .end(function (err, res) {
+              //console.log("res",  res);
+              console.log("res.body", res.body);
+              res.should.have.status(200);
+              res.body.should.be.a('object');
+
+              
+              expect(res.body.success).to.equal(true);
+
+
+              done();
+            });
+        });
+      });
+    });
+  });
+
+
+
+
+  // mocha test/campaignsRoute.js  --grep 'directGroupId'
+
+  it('directGroupId', function (done) {
+
+    var email = "test-message-create-" + Date.now() + "@email.com";
+    var pwd = "pwd";
+
+    userService.signup(email, pwd, "Test Firstname", "Test lastname").then(function (savedUser) {
+      projectService.createAndReturnProjectAndProjectUser("directSimple", savedUser._id).then(function (savedProjectAndPU) {
+
+        var savedProject = savedProjectAndPU.project;
+        
+        var userid = savedUser._id;
+
+        var newGroup = new Group({
+          name: "group1",
+          members: ["userid1", "userid2"],
+          // members: [userid, "userid2"],
+          trashed: false,
+          id_project: savedProject._id,
+          createdBy: userid,
+          updatedBy: userid
+        });
+        newGroup.save(function (err, savedGroup) {
+          console.log("savedGroup", savedGroup)
+
+
+          chai.request(server)
+            .post('/' + savedProject._id + '/campaigns/direct')
+            .auth(email, pwd)
+            .set('content-type', 'application/json')
+            .send({ "text": "ciao", "group_id": savedGroup._id.toString(), returnobject: true })
+            .end(function (err, res) {
+              //console.log("res",  res);
+              console.log("res.body", res.body);
+              res.should.have.status(200);
+              res.body.should.be.a('array');
+
+              expect(res.body.length).to.equal(2);
+              expect(res.body[0].recipient).to.equal("userid1");
+              expect(res.body[1].recipient).to.equal("userid2");
+              // expect(res.body.success).to.equal(true);
+
+
+              done();
+            });
+        });
+      });
+    });
+  });
+
+
+
+
+
+
+});
+
+
