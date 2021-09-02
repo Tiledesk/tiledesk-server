e9e05ad80a9fdd69c68dabfa7235d6f33e3a562f
Andrea Leo <andrea.leo83@gmail.com>
2021-09-02 18:44:28 +0200

    Messages export csv supported

--- routes/messagesRoot.js
+++ routes/messagesRoot.js
@@ -1,11 +1,14 @@
 var express = require('express');
 
 // https://stackoverflow.com/questions/28977253/express-router-undefined-params-with-router-use-when-split-across-files
-var router = express.Router();
+var router = express.Router({mergeParams: true});
 
 var MessageConstants = require("../models/messageConstants");
+var Message = require("../models/message");
 var messageService = require("../services/messageService");
 var winston = require('../config/winston');
+var fastCsv = require("fast-csv");
+var roleChecker = require('../middleware/has-role');
 
 
 router.post('/', 
@@ -39,15 +42,40 @@ router.post('/',
 });
 
 
-//TODO reenable it with role owner
-/*
-router.get('/csv', function(req, res) {
+router.get('/csv', roleChecker.hasRoleOrTypes('owner'), function(req, res) {
 
-  return Message.find({id_project: req.projectid}).sort({createdAt: 'asc'}).exec(function(err, messages) { 
-      if (err) return next(err);
-      res.csv(messages, true);
-    });
+  
+  // return Message.find({id_project: req.projectid}).sort({createdAt: 'asc'}).exec(function(err, messages) { 
+  //     if (err) return next(err);
+  //     res.csv(messages, true);
+  //   });
+
+
+  const cursor = Message.find({id_project: req.projectid}).select("-channel -attributes -metadata");
+
+  const transformer = (doc)=> {
+    return {
+        Id: doc._id,
+        Name: doc.fullname,
+        Email: doc.email,
+        Type: doc.registration_type,
+        RegisterOn: doc.registered_on
+    };
+  }
+
+  const filename = 'export.csv';
+
+  res.setHeader('Content-disposition', `attachment; filename=${filename}`);
+  res.writeHead(200, { 'Content-Type': 'text/csv' });
+
+  res.flushHeaders();
+
+  console.log("fastCsv",fastCsv)
+  var csvStream = fastCsv.format({headers: true})//.transform(transformer)
+  // var csvStream = fastCsv.createWriteStream({headers: true}).transform(transformer)
+  cursor.stream().pipe(csvStream).pipe(res);
+
 });
-*/
+
 
 module.exports = router;
