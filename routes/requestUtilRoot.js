7be71039489d6d43d5af982b8a59678cc296ed62
Andrea Leo <andrea.leo83@gmail.com>
2021-09-06 18:59:02 +0200

    lookup fix

--- routes/requestUtilRoot.js
+++ routes/requestUtilRoot.js
@@ -5,14 +5,19 @@ var router = express.Router();
 var Request = require("../models/request");
 var winston = require('../config/winston');
 
-
+// https://tiledesk-server-pre.herokuapp.com/requests_util/lookup/id_project/support-group-60ffe291f725db00347661ef-b4cb6875785c4a23b27244fe498eecf4
 router.get('/lookup/id_project/:request_id',  function(req, res) {
   winston.info("lookup: "+req.params.request_id);
   
   return Request.findOne({request_id: req.params.request_id}).select("id_project").exec(function(err, request) { 
-      if (err) return next(err);
+      if (err) {
+        return res.status(500).send({success: false, msg: 'Error creating message', err:err });
+      } 
+      if (!request) {
+        return res.status(404).send({success: false, msg: "Request with  " + request_id + " not found" });
+      }
       winston.info("request",request);
-      res.json(request);
+      res.json({id_project: request.id_project});
     });
 
 });
