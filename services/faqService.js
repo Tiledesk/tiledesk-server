11b7b719386bc6311239aac9ba5fcadd262e8dc6
Andrea Leo <andrea.leo83@gmail.com>
2022-04-26 18:30:34 +0200

    handoff bot

--- services/faqService.js
+++ services/faqService.js
@@ -102,6 +102,17 @@ class FaqService {
         ]
 
       }
+
+
+      if (template==="handoff") {
+
+        faqsArray = [         
+          { 'question': '\\start', 'answer': 'Hello', 'intent_display_name': 'start', 'topic': 'internal' },            
+          { 'question': '👨🏻‍🦰 I want an agent', 'answer': 'We are looking for an operator.. '+ActionsConstants.CHAT_ACTION_MESSAGE.AGENT, 'intent_display_name': 'agent_handoff', 'topic': 'internal' },
+          { 'question': 'defaultFallback', 'answer': 'I can not provide an adequate answer. Write a new question or talk to a human agent.\n* Back to start tdAction:start\n* See the docs https://docs.tiledesk.com/\n* 👨🏻‍🦰 I want an agent', 'intent_display_name': 'defaultFallback', 'topic': 'internal' }, //TODO se metto spazio n * nn va              
+        ]
+
+      }
       
       
       faqsArray.forEach(faq => {
