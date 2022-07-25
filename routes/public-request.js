939d244814957263e8f2482767f0a1b0b0ef53bb
Andrea Leo <andrea.leo83@gmail.com>
2022-07-25 16:52:10 +0200

    added download transcript as pdf and txt

--- routes/public-request.js
+++ routes/public-request.js
@@ -3,9 +3,21 @@ var router = express.Router();
 var Message = require("../models/message");
 var Request = require("../models/request");
 var User = require("../models/user");
-var Project = require("../models/project");
-var emailService = require("../services/emailService");
 var winston = require('../config/winston');
+
+var fonts = {
+	Roboto: {
+		normal: 'fonts/Roboto-Regular.ttf',
+		bold: 'fonts/Roboto-Medium.ttf',
+		italics: 'fonts/Roboto-Italic.ttf',
+		bolditalics: 'fonts/Roboto-MediumItalic.ttf'
+	}
+};
+
+var PdfPrinter = require('pdfmake');
+var printer = new PdfPrinter(fonts);
+// var fs = require('fs');
+
 
 
 
@@ -79,6 +91,96 @@ var winston = require('../config/winston');
 
   });
 
+
+  router.get('/:requestid/messages.txt', function(req, res) {
+  
+    winston.debug(req.params);
+    winston.debug("here");    
+    return Message.find({"recipient": req.params.requestid}).sort({createdAt: 'asc'}).exec(function(err, messages) { 
+      if (err) {
+        return res.status(500).send({success: false, msg: 'Error getting object.'});
+      }
+
+      if(!messages){
+        return res.status(404).send({success: false, msg: 'Object not found.'});
+      }
+      
+
+      var text = "Chat transcript:\n" //+ req.project.name;
+      
+      messages.forEach(function(element) {
+        text = text + "[ " + element.createdAt.toLocaleString('en', { timeZone: 'UTC' })+ "] " + element.senderFullname + ": " + element.text + "\n";
+      });
+    
+
+      res.set({"Content-Disposition":"attachment; filename=\"transcript.txt\""});
+      res.send(text);
+    });
+
+  });
+
+
+
+  router.get('/:requestid/messages.pdf', function(req, res) {
+
+
+    winston.debug(req.params);
+    winston.debug("here");    
+    return Message.find({"recipient": req.params.requestid}).sort({createdAt: 'asc'}).exec(function(err, messages) { 
+      if (err) {
+        return res.status(500).send({success: false, msg: 'Error getting object.'});
+      }
+
+
+      if(!messages){
+        return res.status(404).send({success: false, msg: 'Object not found.'});
+      }
+
+
+      var docDefinition = {
+        content: [
+          { text: 'Chat Transcript', style: 'header' },
+          {
+            ul: [
+              // 'item 1',
+              // 'item 2',
+              // 'item 3'
+            ]
+          },
+          
+        ],
+        styles: {
+          header: {
+            bold: true,
+            fontSize: 15
+          }
+        },
+        defaultStyle: {
+          fontSize: 12
+        }
+      };
+
+      
+
+      messages.forEach(function(element) {
+        docDefinition.content[1].ul.push("[ " + element.createdAt.toLocaleString('en', { timeZone: 'UTC' })+ "] " + element.senderFullname + ": " + element.text );
+      });
+
+      console.log(docDefinition);
+   
+    var pdfDoc = printer.createPdfKitDocument(docDefinition);
+    // pdfDoc.pipe(fs.createWriteStream('lists.pdf'));
+    pdfDoc.pipe(res);
+    pdfDoc.end();
+
+
+      
+    });
+
+  });
+
+
+
   router.get('/:requestid/messages-user.html', function(req, res) {
   
     winston.debug(req.params);
@@ -97,6 +199,99 @@ var winston = require('../config/winston');
       }
 
       return res.render('messages', { title: 'Tiledesk', messages: messages});
+    });
+
+  });
+
+
+
+  router.get('/:requestid/messages-user.txt', function(req, res) {
+  
+    winston.debug(req.params);
+    winston.debug("here");    
+    return Message.find({"recipient": req.params.requestid}).sort({createdAt: 'asc'}).exec(function(err, messages) { 
+      if (err) {
+        return res.status(500).send({success: false, msg: 'Error getting object.'});
+      }
+
+      if(!messages){
+        return res.status(404).send({success: false, msg: 'Object not found.'});
+      }
+      
+
+      var messages = messages.filter(m => m.sender != "system" );
+
+      var text = "Chat transcript:\n" //+ req.project.name;
+
+      messages.forEach(function(element) {
+        text = text + "[ " + element.createdAt.toLocaleString('en', { timeZone: 'UTC' })+ "] " + element.senderFullname + ": " + element.text + "\n";
+      });
+    
+
+      res.set({"Content-Disposition":"attachment; filename=\"transcript.txt\""});
+      res.send(text);
+    });
+
+  });
+
+
+  router.get('/:requestid/messages-user.pdf', function(req, res) {
+
+
+    winston.debug(req.params);
+    winston.debug("here");    
+    return Message.find({"recipient": req.params.requestid}).sort({createdAt: 'asc'}).exec(function(err, messages) { 
+      if (err) {
+        return res.status(500).send({success: false, msg: 'Error getting object.'});
+      }
+
+      var messages = messages.filter(m => m.sender != "system" );
+
+
+      //skip info message
+      if(!messages){
+        return res.status(404).send({success: false, msg: 'Object not found.'});
+      }
+
+
+      var docDefinition = {
+        content: [
+          { text: 'Chat Transcript', style: 'header' },
+          {
+            ul: [
+              // 'item 1',
+              // 'item 2',
+              // 'item 3'
+            ]
+          },
+          
+        ],
+        styles: {
+          header: {
+            bold: true,
+            fontSize: 15
+          }
+        },
+        defaultStyle: {
+          fontSize: 12
+        }
+      };
+
+      
+
+      messages.forEach(function(element) {
+        docDefinition.content[1].ul.push("[ " + element.createdAt.toLocaleString('en', { timeZone: 'UTC' })+ "] " + element.senderFullname + ": " + element.text );
+      });
+
+      console.log(docDefinition);
+   
+    var pdfDoc = printer.createPdfKitDocument(docDefinition);
+    // pdfDoc.pipe(fs.createWriteStream('lists.pdf'));
+    pdfDoc.pipe(res);
+    pdfDoc.end();
+
+
+      
     });
 
   });
