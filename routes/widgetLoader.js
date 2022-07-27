cfd8feedd102e8e070563cf74dc60a2c1470617d
Andrea Leo <andrea.leo83@gmail.com>
2022-07-27 13:20:32 +0200

    cache control widget run

--- routes/widgetLoader.js
+++ routes/widgetLoader.js
@@ -12,8 +12,39 @@ router.get('/load', function(req, res) {
   winston.debug(query);
   // TODO chech if query is null
   res.redirect(widgetLocation+'?'+query);
+
 });
   
+
+router.get('/v5/:project_id', function(req, res) {
+
+  var project_id = req.params.project_id;
+  winston.debug("project_id: " + project_id);
+
+  res.type('.js');
+
+  var js = `
+    window.tiledeskSettings= 
+    {
+        projectid: "${project_id}"
+    };
+    (function(d, s, id) { 
+        var w=window; var d=document; var i=function(){i.c(arguments);};
+        i.q=[]; i.c=function(args){i.q.push(args);}; w.Tiledesk=i;                    
+        var js, fjs=d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) return;
+        js=d.createElement(s); 
+        js.id=id; js.async=true; js.src="${widgetLocation}/launch.js";
+        fjs.parentNode.insertBefore(js, fjs);
+    }(document,'script','tiledesk-jssdk'));
+  `;
+
+  winston.debug("js: " + js);
+
+  res.send(js);
+
+});
+
 
 var widgetTestLocation = process.env.WIDGET_TEST_LOCATION || widgetConfig.testLocation;
 
