26ac6f2a29725ab49eb5601dc29d6f73e7333f4a
Andrea Leo <andrea.leo83@gmail.com>
2022-07-27 15:41:03 +0200

    Bugfix  Cannot read property 'profile' of null

--- routes/widget.js
+++ routes/widget.js
@@ -96,6 +96,7 @@ router.get('/', function(req, res, next) {
       winston.debug("req.project:" + JSON.stringify(req.project));
       
       if (req.project) {
+                                    //secondo me qui manca un parentesi tonda per gli or
         if (req.project.profile && (req.project.profile.type === 'free' && req.project.trialExpired === true) || (req.project.profile.type === 'payment' && req.project.isActiveSubscription === false)) {
           query.default = true;
         }
@@ -129,7 +130,21 @@ router.get('/', function(req, res, next) {
             return reject({err: "Project Not Found"});        
           }
 
-          if (project && project.profile && (project.profile.type === 'free' && project.trialExpired === true) || (project.profile.type === 'payment' && project.isActiveSubscription === false)) {
+
+          winston.debug("project", project);
+
+          // ProjectSetter project not found with id: 62d8cf8b2b10b30013bb9b99
+          // Informazioni
+          // 2022-07-27 14:32:14.772 CESTerror: Error getting widget. {"err":"Project Not Found"}
+          // Informazioni
+          // 2022-07-27 14:32:14.778 CESTerror: uncaughtException: Cannot read property 'profile' of null
+          // Informazioni
+          // 2022-07-27 14:32:14.778 CESTTypeError: Cannot read property 'profile' of null at /usr/src/app/routes/widget.js:132:124 at /usr/src/app/node_modules/mongoose/lib/model.js:5074:18 at processTicksAndRejections (internal/process/task_queues.js:79:11) {"date":"Wed Jul 27 2022 12:32:14 GMT+0000 (Coordinated Universal Time)","error":{},"exception":true,"os":{"loadavg":[0.26,0.51,0.58],"uptime":1028128},"process":{"argv":["/usr/local/bin/node","/usr/src/app/bin/www"],"cwd":"/usr/src/app","execPath":"/usr/local/bin/node","gid":0,"memoryUsage":{"arrayBuffers":128833077,"external":130521753,"heapTotal":110641152,"heapUsed":85605912,"rss":310054912},"pid":26,"uid":0,"version":"v12.22.12"},"stack":"TypeError: Cannot read property 'profile' of null\n at /usr/src/app/routes/widget.js:132:124\n at /usr/src/app/node_modules/mongoose/lib/model.js:5074:18\n at processTicksAndRejections (internal/process/task_queues.js:79:11)","trace":[{"column":124,"file":"/usr/src/app/routes/widget.js","function":null,"line":132,"method":null,"native":false},{"column":18,"file":"/usr/src/app/node_modules/mongoose/lib/model.js","function":null,"line":5074,"method":null,"native":false},{"column":11,"file":"internal/process/task_queues.js","function":"processTicksAndRejections","line":79,"method":null,"native":false}]}
+
+          // console.log("project!=null",project!=null);
+          // console.log("project.profile",project.profile);
+                                              //secondo me qui manca un parentesi tonda per gli or
+          if (project  && project.profile && ((project.profile.type === 'free' && project.trialExpired === true) || (project.profile.type === 'payment' && project.isActiveSubscription === false))) {
             winston.debug('getProject remove poweredBy tag', project);
             project.widget.poweredBy = undefined;
             project.widget.baloonImage = undefined;            
