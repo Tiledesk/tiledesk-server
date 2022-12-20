8f8815ba7b6cb7c5bbdac756a271992206651890
Andrea Leo <andrea.leo83@gmail.com>
2022-12-20 18:21:56 +0100

    - chatbot and subscription can use send email endpoint

--- app.js
+++ app.js
@@ -489,7 +489,7 @@ app.use('/:projectid/labels', [fetchLabels],labels);
 
 app.use('/:projectid/campaigns',[passport.authenticate(['basic', 'jwt'], { session: false }), validtoken, roleChecker.hasRole('agent')], campaigns);
 
-app.use('/:projectid/emails',[passport.authenticate(['basic', 'jwt'], { session: false }), validtoken, roleChecker.hasRole('agent')], email);
+app.use('/:projectid/emails',[passport.authenticate(['basic', 'jwt'], { session: false }), validtoken, roleChecker.hasRoleOrTypes('agent', ['bot','subscription'])], email);
 
 
 
