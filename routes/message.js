66d13c7613bf00c1667fca0e72f6ae102f0af7dd
Andrea Leo <andrea.leo83@gmail.com>
2023-01-04 10:10:46 +0100

    disable text validation for send message

--- routes/message.js
+++ routes/message.js
@@ -49,9 +49,10 @@ async (req, res)  => {
   winston.debug('req.params: ', req.params);
   winston.debug('req.params.request_id: ' + req.params.request_id);
 
-  if (!req.body.text &&  (!req.body.type || req.body.type=="text") ) {
-    return res.status(422).json({ errors: ["text field is required"] });
-  } 
+  // sponz: 4/01/23 disable it
+  // if (!req.body.text &&  (!req.body.type || req.body.type=="text") ) {
+  //   return res.status(422).json({ errors: ["text field is required"] });
+  // } 
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
     return res.status(422).json({ errors: errors.array() });
