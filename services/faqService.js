5f36c92d7f3dfd3e61a1e86ca2342fc55e4a475e
Andrea Sponziello <andrea.sponziello@gmail.com>
2023-02-04 10:03:05 +0100

    re-added language to faqsArray

--- services/faqService.js
+++ services/faqService.js
@@ -180,7 +180,7 @@ class FaqService {
           actions: faq.actions,
           reply: faq.reply,
           intent_display_name: faq.intent_display_name,
-          // language: "en",
+          language: "en",
           id_project: projectid,
           topic: faq.topic,
           createdBy: created_by,
