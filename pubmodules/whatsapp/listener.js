e8d56ae4d1b797e4a6637eef51bf7f0c20554d56
Giovanni Troisi <giovanni@tiledesk.com>
2023-02-13 09:39:53 +0100

    update whatsapp connector versione to 0.1.29 update whatsapp listener

--- pubmodules/whatsapp/listener.js
+++ pubmodules/whatsapp/listener.js
@@ -13,20 +13,47 @@ class Listener {
             winston.debug("whatsapp config databaseUri: " + config.databaseUri);
         }
 
-        if (!process.env.GRAPH_URL || !config.graphUrl) {
-            winston.info("Skip WhatsApp startApp")
-        } else {
-            whatsapp.startApp({
-                MONGODB_URL: config.databaseUri,          
-                API_URL: apiUrl,
-                GRAPH_URL: process.env.META_GRAPH_URL || config.graphUrl,
-                BASE_URL: apiUrl + "/modules/whatsapp",                     
-                APPS_API_URL: apiUrl + "/modules/apps",                 
-                log: process.env.WHATSAPP_LOG
-            }, () => {
-                winston.info("Tiledesk WhatsApp Connector proxy server succesfully started.");
-            })
-        }
+        // Johnny
+        let port = process.env.REDIS_PORT;
+        winston.debug("Redis port: "+ port);
+
+        let host = process.env.REDIS_HOST;
+        winston.debug("Redis host: "+ host);
+
+        let password = process.env.REDIS_PASSWORD;
+        winston.debug("Redis password: "+ password);
+        // Johnny - END
+
+        // Andrea
+        // var port = process.env.CACHE_REDIS_PORT || 6379;
+        // winston.info("-Redis port: "+ port);
+
+        // var host = process.env.CACHE_REDIS_HOST || "127.0.0.1"
+        // winston.info("-Redis host: "+ host);
+
+        // var password = process.env.CACHE_REDIS_PASSWORD;
+        // winston.info("-Redis password: "+ password);
+        // Andrea - END
+
+        let graph_url = process.env.GRAPH_URL || config.graphUrl || "https://graph.facebook.com/v14.0/"
+        winston.debug("Whatsapp graph_url: "+ password);
+
+        let log = process.env.WHATSAPP_LOG || false
+        winston.debug("Whatsapp log: "+ log);
+
+        whatsapp.startApp({
+            MONGODB_URL: config.databaseUri,          
+            API_URL: apiUrl,
+            GRAPH_URL: graph_url,
+            BASE_URL: apiUrl + "/modules/whatsapp",                     
+            APPS_API_URL: apiUrl + "/modules/apps",
+            REDIS_HOST: host,
+            REDIS_PORT: port,
+            REDIS_PASSWORD: password,
+            log: log
+        }, () => {
+            winston.info("Tiledesk WhatsApp Connector proxy server succesfully started.");
+        })
 
     }
 }
