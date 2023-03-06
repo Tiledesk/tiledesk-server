9a8aa6bbf76086e69c68e0a794fb2c3b4f4172b2
andrea leo <andrea.leo83@gmail.com>
2023-03-06 17:04:07 +0100

    - Apps module fix for public secret key

--- pubmodules/apps/listener.js
+++ pubmodules/apps/listener.js
@@ -1,6 +1,19 @@
 const apps = require("@tiledesk/tiledesk-apps");
 var winston = require('../../config/winston');
 
+var config = require('../../config/database'); // get db config file
+
+let configSecretOrPubicKay = process.env.GLOBAL_SECRET || config.secret;   
+
+var pKey = process.env.GLOBAL_SECRET_OR_PUB_KEY;
+// console.log("pKey",pKey);
+
+if (pKey) {
+  configSecretOrPubicKay = pKey.replace(/\\n/g, '\n');
+}
+
+// console.log("configSecretOrPubicKay",configSecretOrPubicKay);
+
 class Listener {
 
     listen(config) {
@@ -12,7 +25,7 @@ class Listener {
         }
 
         apps.startApp({
-            ACCESS_TOKEN_SECRET: process.env.APPS_ACCESS_TOKEN_SECRET || 'nodeauthsecret',
+            ACCESS_TOKEN_SECRET: process.env.APPS_ACCESS_TOKEN_SECRET || configSecretOrPubicKay,
             MONGODB_URI: process.env.APPS_MONGODB_URI || config.databaseUri,
             KALEYRA_ENABLED: process.env.KALEYRA_ENABLED || config.kaleyra_enabled
         }, () => {
