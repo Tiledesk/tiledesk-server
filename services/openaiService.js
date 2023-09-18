30844eb40d9e438490984cac94109b32d18b3843
Giovanni Troisi <giovanni@tiledesk.com>
2023-09-18 12:38:30 +0200

    route moved from openai_kb to openai added endpoint to call the open ai completions

---
+++ services/openaiService.js
@@ -0,0 +1,37 @@
+var winston = require('../config/winston');
+const axios = require("axios").default;
+var configGlobal = require('../config/global');
+
+let openai_endpoint = process.env.OPENAI_ENDPOINT;
+
+class OpenaiService {
+
+    completions(data, gptkey) {
+
+        return new Promise((resolve, reject) => {
+
+            axios({
+                url: openai_endpoint + "/chat/completions",
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorizaion': "Bearer " + gptkey
+                },
+                data: data,
+                method: 'POST'
+            }).then((resbody) => {
+                winston.debug("[Openai] completions resbody: ", resbody);
+                resolve(resbody);
+            }).catch((err) => {
+                winston.error("[Openai] completions error: ", err);
+                reject(err);
+            })
+
+        })
+
+    }
+
+}
+
+var openaiService = new OpenaiService();
+
+module.exports = openaiService;
---
+++ test/openaiRoute.js
@@ -0,0 +1,160 @@
+//During the test the env variable is set to test
+process.env.NODE_ENV = 'test';
+
+let log = false;
+var projectService = require('../services/projectService');
+var userService = require('../services/userService');
+
+//Require the dev-dependencies
+let chai = require('chai');
+let chaiHttp = require('chai-http');
+let server = require('../app');
+let should = chai.should();
+var fs = require('fs');
+const path = require('path');
+
+// chai.config.includeStack = true;
+
+var expect = chai.expect;
+var assert = chai.assert;
+
+chai.use(chaiHttp);
+
+describe('FaqKBRoute', () => {
+
+    describe('/create', () => {
+
+        // can't be tested!
+        // it('completions', (done) => {
+
+        //     var email = "test-signup-" + Date.now() + "@email.com";
+        //     var pwd = "pwd";
+
+        //     userService.signup(email, pwd, "Test Firstname", "Test Lastname").then((savedUser) => {
+        //         projectService.create("test-openai-create", savedUser._id).then((savedProject) => {
+
+        //             chai.request(server)
+        //                 .post('/' + savedProject._id + '/kbsettings')
+        //                 .auth(email, pwd)
+        //                 .send({}) // can be empty
+        //                 .end((err, res) => {
+        //                     if (log) { console.log("create kbsettings res.body: ", res.body); }
+        //                     res.should.have.status(200);
+        //                     res.body.should.be.a('object');
+
+        //                     chai.request(server)
+        //                         .put('/' + savedProject._id + "/kbsettings/" + res.body._id)
+        //                         .auth(email, pwd)
+        //                         .send({ gptkey: "sk-12345678" })
+        //                         .end((err, res) => {
+        //                             if (log) { console.log("add kb to kb settings res.body: ", res.body); }
+        //                             res.should.have.status(200);
+        //                             res.body.should.be.a('object');
+
+        //                             chai.request(server)
+        //                                 .post('/' + savedProject._id + '/openai')
+        //                                 .auth(email, pwd)
+        //                                 .send({ question: "Provide 3 names for a dog", context: "you are an awesome assistant", max_tokens: 100, temperature: 0, model: "gpt-3.5-turbo" })
+        //                                 .end((err, res) => {
+        //                                     console.log("res.body: ", res.body);
+        //                                     console.log("res.status: ", res.status);
+
+        //                                     done();
+        //                                 })
+
+        //                         })
+        //                 })
+
+
+        //             chai.request(server)
+        //                 .post('/' + savedProject._id + '/kbsettings')
+        //                 .auth(email, pwd)
+        //                 .send({}) // can be empty
+        //                 .end((err, res) => {
+        //                     if (log) { console.log("create kbsettings res.body: ", res.body); }
+        //                     res.should.have.status(200);
+        //                     res.body.should.be.a('object');
+        //                     expect(res.body.id_project).to.equal(savedProject._id.toString());
+
+        //                     chai.request(server)
+        //                         .post('/' + savedProject._id + '/openai')
+        //                         .auth(email, pwd)
+        //                         .send({ question: "Provide 3 names for a dog", context: "you are an awesome assistant", max_tokens: 100, temperature: 0, model: "gpt-3.5-turbo" })
+        //                         .end((err, res) => {
+        //                             console.log("res.body: ", res.body);
+        //                             console.log("res.status: ", res.status);
+
+        //                             done();
+        //                         })
+        //                 })
+
+
+        //         })
+        //     })
+        // }).timeout(20000)
+
+        it('completions missing gptkey', (done) => {
+
+            var email = "test-signup-" + Date.now() + "@email.com";
+            var pwd = "pwd";
+
+            userService.signup(email, pwd, "Test Firstname", "Test Lastname").then((savedUser) => {
+                projectService.create("test-openai-create", savedUser._id).then((savedProject) => {
+
+                    chai.request(server)
+                        .post('/' + savedProject._id + '/kbsettings')
+                        .auth(email, pwd)
+                        .send({}) // can be empty
+                        .end((err, res) => {
+                            if (log) { console.log("create kbsettings res.body: ", res.body); }
+                            res.should.have.status(200);
+                            res.body.should.be.a('object');
+
+                            chai.request(server)
+                                .post('/' + savedProject._id + '/openai')
+                                .auth(email, pwd)
+                                .send({ question: "Provide 3 names for a dog", context: "you are an awesome assistant", max_tokens: 100, temperature: 0, model: "gpt-3.5-turbo" })
+                                .end((err, res) => {
+                                    if (log) {console.log("res.body: ", res.body);}
+                                    res.should.have.status(400);
+                                    res.body.should.be.a('object');
+                                    expect(res.body.success).to.equal(false);
+                                    expect(res.body.message).to.equal("Missing gptkey parameter");
+
+                                    done();
+                                })
+                        })
+                })
+            })
+        }).timeout(20000)
+
+        it('completions missing settings', (done) => {
+
+            var email = "test-signup-" + Date.now() + "@email.com";
+            var pwd = "pwd";
+
+            userService.signup(email, pwd, "Test Firstname", "Test Lastname").then((savedUser) => {
+                projectService.create("test-openai-create", savedUser._id).then((savedProject) => {
+
+                    chai.request(server)
+                        .post('/' + savedProject._id + '/openai')
+                        .auth(email, pwd)
+                        .send({ question: "Provide 3 names for a dog", context: "you are an awesome assistant", max_tokens: 100, temperature: 0, model: "gpt-3.5-turbo" })
+                        .end((err, res) => {
+                            if (log) {console.log("res.body: ", res.body);}
+                            res.should.have.status(400);
+                            res.body.should.be.a('object');
+                            expect(res.body.success).to.equal(false);
+                            expect(res.body.message).to.equal("Missing gptkey parameter (settings not exist)");
+
+                            done();
+                        })
+                })
+            })
+        }).timeout(20000)
+
+    });
+
+});
+
+
