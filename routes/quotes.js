c6543fba6610708dae0565e039ff9545af7979c9
Giovanni Troisi <giovanni@tiledesk.com>
2024-01-02 18:31:53 +0100

    added endpoint to get quote availability

--- routes/quotes.js
+++ routes/quotes.js
@@ -10,16 +10,31 @@ router.get('/', async (req, res) => {
     let obj = { createdAt: new Date() };
     if (date) {
         obj.createdAt = new Date(date)
-    }    
-    
+    }
+
     let cache = req.app.get('redis_client');
 
     let qm = new QuoteManager({ project: req.project, tdCache: cache })
     let quotes = await qm.getAllQuotes(obj);
 
     winston.debug("quotes: ", quotes);
-    res.status(200).send({ message: 'ok', quotes: quotes});
+    res.status(200).send({ message: 'ok', quotes: quotes });
 
+})
+
+router.get('/:type', async (req, res) => {
+
+    let type = req.params.type;
+    let obj = { createdAt: new Date() };
+
+    let cache = req.app.get('redis_client');
+
+    let qm = new QuoteManager({ project: req.project, tdCache: cache })
+    let isAvailable = await qm.checkQuote(obj, type);
+
+    winston.debug("is " + type + " available: ", isAvailable);
+    res.status(200).send({ isAvailable: isAvailable })
+    
 })
 
 module.exports = router;
