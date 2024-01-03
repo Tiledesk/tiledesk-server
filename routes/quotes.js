180442267836189c6db5aaa45b0cf823de99de81
Giovanni Troisi <giovanni@tiledesk.com>
2024-01-03 14:44:31 +0100

    added incr endpoint in quotes route

--- routes/quotes.js
+++ routes/quotes.js
@@ -11,8 +11,8 @@ router.post('/', async (req, res) => {
     let obj = { createdAt: new Date() };
     if (date) {
         obj.createdAt = new Date(date)
-    }    
-    
+    }
+
     let quoteManager = req.app.get('quote_manager');
 
     // check if project is not null/undefined
@@ -26,17 +26,27 @@ router.post('/', async (req, res) => {
 router.get('/:type', async (req, res) => {
 
     let type = req.params.type;
-    console.log("type: ", type)
     let obj = { createdAt: new Date() };
 
-    let cache = req.app.get('redis_client');
-
-    let qm = new QuoteManager({ tdCache: cache })
-    let isAvailable = await qm.checkQuote(req.project, obj, type);
+    let quoteManager = req.app.get('quote_manager');
+    let isAvailable = await quoteManager.checkQuote(req.project, obj, type);
 
     winston.debug("is " + type + " available: ", isAvailable);
     res.status(200).send({ isAvailable: isAvailable })
-    
+
+})
+
+router.post('/incr/:type', async (req, res) => {
+
+    let type = req.params.type;
+    let body = req.body;
+    body.createdAt = new Date(body.createdAt);
+
+    let quoteManager = req.app.get('quote_manager');
+    let incremented_key = await quoteManager.incrementTokenCount(req.project, req.body);
+    let quote = await quoteManager.getCurrentQuote(req.project, req.body, type);
+
+    res.status(200).send({ message: "value incremented for key " + incremented_key, key: incremented_key, currentQuote: quote });
 })
 
 module.exports = router;
