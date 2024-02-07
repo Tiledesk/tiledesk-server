b08bd0a08071afe7770943ae7c85157e0478f8e4
Giovanni Troisi <giovanni@tiledesk.com>
2024-02-07 13:27:53 +0100

    update

--- routes/kbsettings.js
+++ routes/kbsettings.js
@@ -148,28 +148,28 @@ router.post('/checkstatus', async (req, res) => {
 
         let response = resp.data[full_url];
 
-        // let return_data = {
-        //     status_message: response.status_message
-        // }
+        let return_data = {
+            status_message: response.status_message
+        }
 
-        // if (response.status_code === 3) {
-        //     return_data.status_code = 2;
-        // }
+        if (response.status_code === 3) {
+            return_data.status_code = 2;
+        }
 
-        // if (response.status_code === 1 || response.status_code === 2 ) {
-        //     return_data.status_code = 1;
-        // }
+        if (response.status_code === 1 || response.status_code === 2 ) {
+            return_data.status_code = 1;
+        }
 
-        // if (response.status_code === 0) {
-        //     return_data.status_code = 0;
-        // }
+        if (response.status_code === 0) {
+            return_data.status_code = 0;
+        }
 
-        // if (!response.status_code) {
-        //     return_data.status_code = 0;
-        // }
+        if (!response.status_code) {
+            return_data.status_code = 0;
+        }
 
         
-        res.status(200).send(response);
+        res.status(200).send(return_data);
     }).catch((err) => {
         // console.log("checkstatus err: ", err)
         //winston.error("checkstatus err: ", err);
