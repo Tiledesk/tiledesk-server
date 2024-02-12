7eb316378d64d44fb31685b04d1758d4e7e7f8e4
Giovanni Troisi <giovanni@tiledesk.com>
2024-02-12 17:59:33 +0100

    udpated mqttTest route updated tiledesk-client to 0.10.10

--- pubmodules/mqttTest/listener.js
+++ pubmodules/mqttTest/listener.js
@@ -9,20 +9,11 @@ class Listener {
 
     listen(config) {
         winston.info("MqttTest Listener listen");
-       
-        let chat_api_endpoint = process.env.MQTT_APIENDPOINT;
-        winston.info("chat_api_endpoint: " + chat_api_endpoint);
-
-        let mqtt_endpoint = process.env.MQTT_ENDPOINT;
-        winston.info("mqtt_endpoint: " + mqtt_endpoint);
-
+    
         let log = process.env.MQTT_TEST_LOG || false;
         winston.info("mqtt log: " + log);
 
         mqttTest.startApp({
-            CHAT_API_ENDPOINT: chat_api_endpoint,
-            MQTT_ENDPOINT: mqtt_endpoint,
-            API_ENDPOINT: apiUrl,
             LOG_STATUS: log
         }, (err) => {
             if (!err) {
