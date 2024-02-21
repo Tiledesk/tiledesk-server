02a64a13ffd4811a86be1bd6fe591778e0deb05f
Giovanni Troisi <giovanni@tiledesk.com>
2024-02-21 17:34:28 +0100

    added trainingQueue in app.js

--- app.js
+++ app.js
@@ -197,6 +197,10 @@ let whatsappQueue = require('@tiledesk/tiledesk-whatsapp-jobworker');
 winston.info("whatsappQueue");
 jobsManager.listenWhatsappQueue(whatsappQueue);
 
+let trainingQueue = require('@tiledesk/tiledesk-train-jobworker');
+winston.info("trainingQueue");
+jobsManager.listenTrainingQueue(trainingQueue);
+
 
 var channelManager = require('./channels/channelManager');
 channelManager.listen(); 
