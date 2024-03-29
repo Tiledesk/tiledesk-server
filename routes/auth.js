4e6759460bf5603081f749d9ba95d2081e336d51
Giovanni Troisi <giovanni@tiledesk.com>
2024-03-29 15:46:18 +0100

    restored signup code

--- routes/auth.js
+++ routes/auth.js
@@ -75,29 +75,26 @@ router.post('/signup',
         
         winston.debug('-- >> -- >> savedUser ', savedUser.toObject());
 
+        // let skipVerificationEmail = false;
+        // if (req.headers.authorization) {
 
-        // la signup non prende un token, ma se viene passato devo controllare se questo appartiene al superadmin.
-        // se è superadmin non bisogna mandare l'email di verifica
-        // un token che non è superadmin -> ignoriamo
+        //   let token = req.headers.authorization.split(" ")[1];
+        //   let decode = jwt.verify(token, configSecret)
+        //   if (decode && (decode.email === process.env.ADMIN_EMAIL)) {
+        //     skipVerificationEmail = true;
+        //     winston.verbose("skip sending verification email")
+        //   }
+        // }
 
-        let skipVerificationEmail = false;
-        if (req.headers.authorization) {
-
-          let token = req.headers.authorization.split(" ")[1];
-          let decode = jwt.verify(token, configSecret)
-          if (decode && (decode.email === process.env.ADMIN_EMAIL)) {
-            skipVerificationEmail = true;
-            winston.verbose("skip sending verification email")
-          }
-        }
-
-
-        if (!req.body.disableEmail){
-          if (!skipVerificationEmail) {
-            emailService.sendVerifyEmailAddress(savedUser.email, savedUser);
-          }
-        }
+        // if (!req.body.disableEmail){
+        //   if (!skipVerificationEmail) {
+        //     emailService.sendVerifyEmailAddress(savedUser.email, savedUser);
+        //   }
+        // }
         
+        if (!req.body.disableEmail){
+            emailService.sendVerifyEmailAddress(savedUser.email, savedUser);
+        }
 
 
         /*
