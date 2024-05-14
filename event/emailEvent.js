813a4bc9dcabe821d4a78bc8f9a0b38ac06d4114
Giovanni Troisi <giovanni@tiledesk.com>
2024-05-14 13:05:42 +0200

    restored emailEvent for test

--- event/emailEvent.js
+++ event/emailEvent.js
@@ -1,72 +1,87 @@
+
+
 const EventEmitter = require('events');
-const emailService = require('../services/emailService');
-const project_user = require('../models/project_user');
-var winston = require('../config/winston');
-const user = require('../models/user');
 
 class EmailEvent extends EventEmitter {
     constructor() {
         super();
         this.queueEnabled = false;
     }
-
-    listen() {
-
-        emailEvent.on('email.send.quote.checkpoint', function (data) {
-
-            // TODO setImmediate here?
-            winston.debug("emailEvent data: ", data);
-
-            project_user.findOne({ id_project: data.id_project }, (err, puser) => {
-
-                if (err) {
-                    winston.error("error finding owner user: " + err);
-                    return;
-                }
-
-                if (!puser) {
-                    winston.error("Owner user not found. Unable to send checkpoint quota reached.");
-                    return;
-                }
-
-                user.findOne({ _id: puser.id_user }, (err, user) => {
-
-                    if (err) {
-                        winston.error("Error finding user: ", err);
-                        return
-                    }
-
-                    if (!user) {
-                        winston.error("User not found. Unable to send checkpoint quota reached.")
-                        return;
-                    }
-
-                    let resource_name;
-                    if (data.type == 'requests') {
-                        resource_name = 'Conversations'
-                    }
-                    if (data.type == 'tokens') {
-                        resource_name = 'AI Tokens'
-                    }
-                    if (data.type == 'email') {
-                        resource_name = 'Chatbot Email'
-                    }
-
-                    emailService.sendEmailQuotaCheckpointReached(user.email, user.firstname, data.project_name, resource_name, data.checkpoint, data.quotes);
-
-                })
-
-
-            })
-
-        });
-
-        emailEvent.on('email.send.quote', function (data) {
-            console.log("email.send.quote event catched in emailEvent")
-        })
-    }
 }
 
 const emailEvent = new EmailEvent();
 
-module.exports = emailEvent;
+module.exports =  emailEvent;
+
+// const EventEmitter = require('events');
+// const emailService = require('../services/emailService');
+// const project_user = require('../models/project_user');
+// var winston = require('../config/winston');
+// const user = require('../models/user');
+
+// class EmailEvent extends EventEmitter {
+//     constructor() {
+//         super();
+//         this.queueEnabled = false;
+//     }
+
+//     listen() {
+
+//         emailEvent.on('email.send.quote.checkpoint', function (data) {
+
+//             // TODO setImmediate here?
+//             winston.debug("emailEvent data: ", data);
+
+//             project_user.findOne({ id_project: data.id_project }, (err, puser) => {
+
+//                 if (err) {
+//                     winston.error("error finding owner user: " + err);
+//                     return;
+//                 }
+
+//                 if (!puser) {
+//                     winston.error("Owner user not found. Unable to send checkpoint quota reached.");
+//                     return;
+//                 }
+
+//                 user.findOne({ _id: puser.id_user }, (err, user) => {
+
+//                     if (err) {
+//                         winston.error("Error finding user: ", err);
+//                         return
+//                     }
+
+//                     if (!user) {
+//                         winston.error("User not found. Unable to send checkpoint quota reached.")
+//                         return;
+//                     }
+
+//                     let resource_name;
+//                     if (data.type == 'requests') {
+//                         resource_name = 'Conversations'
+//                     }
+//                     if (data.type == 'tokens') {
+//                         resource_name = 'AI Tokens'
+//                     }
+//                     if (data.type == 'email') {
+//                         resource_name = 'Chatbot Email'
+//                     }
+
+//                     emailService.sendEmailQuotaCheckpointReached(user.email, user.firstname, data.project_name, resource_name, data.checkpoint, data.quotes);
+
+//                 })
+
+
+//             })
+
+//         });
+
+//         emailEvent.on('email.send.quote', function (data) {
+//             console.log("email.send.quote event catched in emailEvent")
+//         })
+//     }
+// }
+
+// const emailEvent = new EmailEvent();
+
+// module.exports = emailEvent;
