8957c3e0ecef5eb789335d0ae749da4815ab131b
Giovanni Troisi <giovanni@tiledesk.com>
2024-05-16 11:54:55 +0200

    updated /kb/qa endpint with checkQuote and incremenTokenCount

--- routes/kb.js
+++ routes/kb.js
@@ -447,7 +447,10 @@ router.post('/scrape/status', async (req, res) => {
 })
 
 router.post('/qa', async (req, res) => {
+
+    let publicKey = false;
     let data = req.body;
+    
     // add or override namespace value if it is passed for security reason
     data.namespace = req.projectid;
     winston.debug("/qa data: ", data);
@@ -462,6 +465,17 @@ router.post('/qa', async (req, res) => {
             return res.status(403).send({ success: false, error: "GPT apikey undefined" })
         }
         data.gptkey = gptkey;
+        publicKey = true;
+    }
+
+    let obj = { createdAt: new Date() };
+
+    let quoteManager = req.app.get('quote_manager');
+    if (publicKey === true) {
+        let isAvailable = await quoteManager.checkQuote(req.project, obj, 'tokens');
+        if (isAvailable === false) {
+            return res.status(403).send("Tokens quota exceeded")
+        }
     }
 
     openaiService.askNamespace(data).then((resp) => {
@@ -474,7 +488,20 @@ router.post('/qa', async (req, res) => {
             id = id.substring(index + 1);
         }
 
+        console.log("askNamespace resp: ", resp);
+
         KB.findById(id, (err, resource) => {
+
+            let multiplier = MODEL_MULTIPLIER[data.model];
+            if (!multiplier) {
+                multiplier = 1;
+                winston.info("No multiplier found for AI model")
+            }
+            obj.multiplier = multiplier;
+            obj.tokens = resp.prompt_token_size;
+
+            let incremented_key = quoteManager.incrementTokenCount(req.project, obj);
+            winston.verbose("incremented_key: ", incremented_key);
 
             if (err) {
                 winston.error("Unable to find resource with id " + id + " in namespace " + answer.namespace + ". The standard answer is returned.")
--- routes/quotes.js
+++ routes/quotes.js
@@ -40,20 +40,20 @@ router.get('/:type', async (req, res) => {
 router.post('/incr/:type', async (req, res) => {
 
     let type = req.params.type;
-    let body = req.body;
-    body.createdAt = new Date();
+    let data = req.body;
 
     let quoteManager = req.app.get('quote_manager');
 
-    let multiplier = MODEL_MULTIPLIER[body.model];
+    let multiplier = MODEL_MULTIPLIER[data.model];
     if (!multiplier) {
         multiplier = 1;
         winston.info("No multiplier found for AI model")
     }
-    body.multiplier = multiplier;
+    data.multiplier = multiplier;
+    data.createdAt = new Date();
 
-    let incremented_key = await quoteManager.incrementTokenCount(req.project, body);
-    let quote = await quoteManager.getCurrentQuote(req.project, req.body, type);
+    let incremented_key = await quoteManager.incrementTokenCount(req.project, data);
+    let quote = await quoteManager.getCurrentQuote(req.project, data, type);    
 
     res.status(200).send({ message: "value incremented for key " + incremented_key, key: incremented_key, currentQuote: quote });
 })
