11e923a6fedb1a1d98a4eb3997c9afe04b700e5d
Giovanni Troisi <giovanni@tiledesk.com>
2024-06-14 09:51:53 +0200

    update

--- utils/aiUtils.js
+++ utils/aiUtils.js
@@ -9,9 +9,7 @@ var winston = require('../config/winston');
 loadMultiplier();
 function loadMultiplier() {
     
-    console.log("process.env.AI_MODELS: ", process.env.AI_MODELS)
-    
-    let models_string = process.env.MODELS.replace(/ /g,'');
+    let models_string = process.env.AI_MODELS.replace(/ /g,'');
     winston.debug("(loadMultiplier) models_string: ", models_string)
 
     let splitted_string = models_string.split(";");
