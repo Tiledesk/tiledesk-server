353a618ae35151fba44acc34dfc918982aa9df60
Giovanni Troisi <giovanni@tiledesk.com>
2024-08-28 10:27:17 +0200

    updated logs

--- app.js
+++ app.js
@@ -424,6 +424,7 @@ if (process.env.ROUTELOGGER_ENABLED==="true") {
 
 app.get('/', function (req, res) {  
   console.log("x-forwarded-for: ", req.headers['x-forwarded-for'], "remote port: ", req.socket.remotePort);
+  console.log("X-Real-IP ", req.headers['X-Real-IP'])
   console.log("remoteAddress: ",  req.socket.remoteAddress)
   res.send('Hello from Tiledesk server. It\'s UP. See the documentation here http://developer.tiledesk.com');
 });
