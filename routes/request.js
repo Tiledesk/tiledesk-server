ba44266832dc3d73622c1887ba3eb90b40c030b3
Giovanni Troisi <giovanni@tiledesk.com>
2024-09-03 17:03:40 +0200

    added /count endpoint for request route

--- routes/request.js
+++ routes/request.js
@@ -1517,5 +1517,50 @@ router.get('/:requestid/chatbot/parameters', async (req, res) => {
 
 })
 
+router.get('/count', async (req, res) => {
+  
+  let id_project = req.projectid;
+  let merge_assigned = req.query.merge_assigned;
+
+  let unassigned_count = 0;
+  let human_assigned_count = 0;
+  let bot_assigned_count = 0;
+
+  unassigned_count = await Request.countDocuments({ id_project: id_project, status: 100, preflight: false }).catch((err) => {
+    winston.error("Error getting unassigned requests count: ", err);
+    res.status(400).send({ success: false, error: err });
+  })
+  winston.info("Unassigned count: ", unassigned_count);
+
+  human_assigned_count = await Request.countDocuments({ id_project: id_project, status: 200, preflight: false, hasBot: false }).catch((err) => {
+    winston.error("Error getting human unassigned requests count: ", err);
+    res.status(400).send({ success: false, error: err });
+  })
+  winston.info("Unassigned count: ", unassigned_count);
+
+  bot_assigned_count = await Request.countDocuments({ id_project: id_project, status: 200, preflight: false, hasBot: true }).catch((err) => {
+    winston.error("Error getting bot assigned requests count: ", err);
+    res.status(400).send({ success: false, error: err });
+  })
+  winston.info("Unassigned count: ", bot_assigned_count);
+
+
+  if (merge_assigned) {
+    let data = {
+      unassigned: unassigned_count,
+      assigned: human_assigned_count + bot_assigned_count
+    }
+    return res.status(200).send(data);
+    
+  } else {
+    let data = {
+      unassigned: unassigned_count,
+      assigned: human_assigned_count,
+      bot_assigned: bot_assigned_count
+    }
+    return res.status(200).send(data);
+  }
+
+})
 
 module.exports = router;
