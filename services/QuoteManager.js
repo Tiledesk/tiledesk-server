4b87b60a6562defdca52a58d35ee980ce8610abe
Giovanni Troisi <giovanni@tiledesk.com>
2024-09-23 09:43:29 +0200

    changed data format

--- services/QuoteManager.js
+++ services/QuoteManager.js
@@ -135,7 +135,7 @@ class QuoteManager {
         }
         winston.debug("renewalDate: ", renewalDate)
 
-        return "quotes:" + type + ":" + this.project._id + ":" + renewalDate.format('DD/MM/YYYY');
+        return "quotes:" + type + ":" + this.project._id + ":" + renewalDate.format('M/D/YY');
         // return "quotes:" + type + ":" + this.project._id + ":" + renewalDate.format('MM/DD/YYYY');
         // return "quotes:" + type + ":" + this.project._id + ":" + renewalDate.toLocaleString();
     }
