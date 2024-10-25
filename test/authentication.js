23b0242c8b3b64b6ce4189d5772b9bf2c7200cd3
Giovanni Troisi <giovanni@tiledesk.com>
2024-10-25 17:02:00 +0200

    Added regex on password on signup and changepsw Added control on same password on changepsw Remove mongo error from signup response (user already exists)

--- test/authentication.js
+++ test/authentication.js
@@ -186,7 +186,7 @@ describe('/signup', () => {
     //   this.timeout();
 
        var email = "test-signuook-" + Date.now() + "@email.com";
-       var pwd = "pwd";
+       var pwd = "Pwd1234!";
 
           
                     chai.request(server)
@@ -244,7 +244,7 @@ describe('/signup', () => {
         //   this.timeout();
             var now = Date.now();
            var email = "test-signupUpperCaseEmail-" + now + "@email.com";
-           var pwd = "pwd";
+           var pwd = "Pwd1234!";
     
               
                         chai.request(server)
@@ -272,7 +272,7 @@ describe('/signup', () => {
         //   this.timeout();
     
            var email = "test-signuoOk-" + Date.now() + "@email";
-           var pwd = "pwd";
+           var pwd = "Pwd1234!";
     
               
                         chai.request(server)
