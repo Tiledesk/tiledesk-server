ec157874525ee03738b384c9a49db095641a47d7
Giovanni Troisi <giovanni@tiledesk.com>
2024-11-13 17:03:39 +0100

    added route for twilio-voice

--- pubmodules/voice-twilio/index.js
+++ pubmodules/voice-twilio/index.js
@@ -1,6 +1,6 @@
 const listener = require("./listener");
 
-const twilio_voice = require("@tiledesk/tiledesk-")
+const twilio_voice = require('@tiledesk/tiledesk-voice-twilio-connector');
 const twilioVoiceRoute = twilio_voice.router;
 
 module.exports = { listener: listener, twilioVoiceRoute: twilioVoiceRoute }
