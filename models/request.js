87e7a7ec3d94121e224a0f0ae87462e00e4ecadc
Giovanni Troisi <giovanni@tiledesk.com>
2024-11-13 09:54:35 +0100

    added indexes to request model

--- models/request.js
+++ models/request.js
@@ -492,6 +492,12 @@ RequestSchema.index({ id_project: 1, createdAt: 1, preflight: 1});
 //suggested by atlas profiler. Used by auto closing requests
 RequestSchema.index({ hasBot: 1, status: 1, createdAt: 1});
 
+// Evaluate following indexes
+RequestSchema.index({ "channel.name": 1, id_project: 1, preflight: 1, "snapshot.requester.uuid_user": 1, createdAt: - 1, status: 1 })
+RequestSchema.index({ id_project: 1, preflight: 1, "snapshot.agents.id_user": 1, updatedAt: -1, draft: 1, status: 1 })
+RequestSchema.index({ id_project: 1, participants: 1, preflight: 1, updatedAt: -1, draft: 1, status: 1 })
+RequestSchema.index({ id_project: 1, preflight: 1, updatedAt: -1, draft: 1, status: 1 })
+
 
 //   cannot index parallel arrays [agents] [participants] {"driv
 // RequestSchema.index({ id_project: 1, status: 1, preflight:1, participants:1, "agents.id_user":1, updatedAt: -1 }); //NN LO APPLICA
