50c3ad6537499f15ae3df984ee0c7f6aece698de
Giovanni Troisi <giovanni@tiledesk.com>
2024-12-11 11:20:43 +0100

    updated PLANS_LIST

--- services/QuoteManager.js
+++ services/QuoteManager.js
@@ -16,12 +16,12 @@ const emailEvent = require('../event/emailEvent');
 
 
 const PLANS_LIST = {
-    FREE_TRIAL: { requests: 200,    messages: 0,    tokens: 100000,     voice: 0,       email: 200,     chatbots: 20,      namespace: 3,   kbs: 50     }, // same as PREMIUM
-    SANDBOX:    { requests: 200,    messages: 0,    tokens: 100000,     voice: 0,       email: 200,     chatbots: 2,       namespace: 1,   kbs: 50     },
-    BASIC:      { requests: 800,    messages: 0,    tokens: 2000000,    voice: 0,       email: 200,     chatbots: 5,       namespace: 1,   kbs: 150    },
-    PREMIUM:    { requests: 3000,   messages: 0,    tokens: 5000000,    voice: 0,       email: 200,     chatbots: 20,      namespace: 3,   kbs: 300    },
-    TEAM:       { requests: 5000,   messages: 0,    tokens: 10000000,   voice: 0,       email: 200,     chatbots: 50,      namespace: 10,  kbs: 1000   },
-    CUSTOM:     { requests: 5000,   messages: 0,    tokens: 10000000,   voice: 120000,  email: 200,     chatbots: 50,      namespace: 10,  kbs: 1000   },
+    FREE_TRIAL: { requests: 200,    messages: 0,    tokens: 100000,     voice_duration: 0,       email: 200,     chatbots: 20,      namespace: 3,   kbs: 50     }, // same as PREMIUM
+    SANDBOX:    { requests: 200,    messages: 0,    tokens: 100000,     voice_duration: 0,       email: 200,     chatbots: 2,       namespace: 1,   kbs: 50     },
+    BASIC:      { requests: 800,    messages: 0,    tokens: 2000000,    voice_duration: 0,       email: 200,     chatbots: 5,       namespace: 1,   kbs: 150    },
+    PREMIUM:    { requests: 3000,   messages: 0,    tokens: 5000000,    voice_duration: 0,       email: 200,     chatbots: 20,      namespace: 3,   kbs: 300    },
+    TEAM:       { requests: 5000,   messages: 0,    tokens: 10000000,   voice_duration: 0,       email: 200,     chatbots: 50,      namespace: 10,  kbs: 1000   },
+    CUSTOM:     { requests: 5000,   messages: 0,    tokens: 10000000,   voice_duration: 120000,  email: 200,     chatbots: 50,      namespace: 10,  kbs: 1000   },
 }
 
 const typesList = ['requests', 'messages', 'email', 'tokens', 'chatbots', 'kbs']
