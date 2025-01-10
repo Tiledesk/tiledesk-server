5bea0d854b9d077ae4687ab1a4383572459a8117
Giovanni Troisi <giovanni@tiledesk.com>
2025-01-10 15:55:22 +0100

    update

--- routes/request.js
+++ routes/request.js
@@ -23,7 +23,7 @@ var cacheEnabler = require("../services/cacheEnabler");
 var Project_user = require("../models/project_user");
 var Lead = require("../models/lead");
 var UIDGenerator = require("../utils/UIDGenerator");
-let { Publisher } = require("@tiledesk/tiledesk-multi-worker");
+let JobManager = require("@tiledesk/tiledesk-multi-worker");
 
 csv = require('csv-express');
 csv.separator = ';';
@@ -39,7 +39,7 @@ const faq_kb = require('../models/faq_kb');
 
 const AMQP_MANAGER_URL = process.env.AMQP_MANAGER_URL;
 
-let jobManager = new Publisher(AMQP_MANAGER_URL, {
+let jobManager = new JobManager(AMQP_MANAGER_URL, {
   debug: false,
   queueName: "conversation-tags_queue",
   exchange: "tiledesk-multi",
