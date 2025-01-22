142591614d4e02058c29a1d3e4ff6c49d8dff0b4
Giovanni Troisi <giovanni@tiledesk.com>
2025-01-22 15:35:41 +0100

    update

--- routes/faq_kb.js
+++ routes/faq_kb.js
@@ -245,10 +245,6 @@ router.put('/:faq_kbid/publish', roleChecker.hasRole('admin'), async (req, res) 
     let forkedChatBotId = forked.bot_id;
     winston.debug("forkedChatBotId: "+forkedChatBotId);
 
-
-    let update = {
-
-    }
     let updatedForkedChabot = await Faq_kb.findByIdAndUpdate(forkedChatBotId, { $unset: { slug: 1 }, trashed: true, publishedBy: req.user.id, publishedAt: new Date().getTime()}, { new: true, upsert: true }).exec();
     winston.debug("updatedForkedChabot: ",updatedForkedChabot);
     botEvent.emit('faqbot.update', updatedForkedChabot);
