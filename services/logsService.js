7bd93f7336c317257ac4282d108933d3c6f7d323
Giovanni Troisi <giovanni@tiledesk.com>
2025-03-18 18:11:48 +0100

    updated logsService

--- services/logsService.js
+++ services/logsService.js
@@ -3,14 +3,20 @@
 const { FlowLogs } = require("../models/flowLogs");
 const default_log_level = 'info';
 
+const levels = { error: 0, warn: 1, info: 2, debug: 3 };
+
 class LogsService {
 
     async getLastRows(request_id, limit, logLevel) {
         let level = logLevel || default_log_level;
+        if (level === 'default') {
+            level = default_log_level
+        }
+        let nlevel = levels[level];
         return FlowLogs.aggregate([
             { $match: { request_id: request_id } },
             { $unwind: "$rows" },
-            { $match: { "rows.level": level } },
+            { $match: { "rows.level": { $lt: nlevel } } },
             { $sort: { "rows.timestamp": -1, "rows._id": -1 } },
             { $limit: limit }
         ]).then(rows => rows.reverse())
@@ -18,10 +24,14 @@ class LogsService {
 
     async getOlderRows(request_id, limit, logLevel, timestamp) {
         let level = logLevel || default_log_level;
+        if (level === 'default') {
+            level = default_log_level
+        }
+        let nlevel = levels[level];
         return FlowLogs.aggregate([
             { $match: { request_id: request_id } },
             { $unwind: "$rows" },
-            { $match: { "rows.level": level, "rows.timestamp": { $lt: timestamp } } },
+            { $match: { "rows.level": { $lt: nlevel }, "rows.timestamp": { $lt: timestamp } } },
             { $sort: { "rows.timestamp": -1, "rows._id": -1 } },
             { $limit: limit }
         ]).then(rows => rows.reverse())
@@ -29,10 +39,14 @@ class LogsService {
 
     async getNewerRows(request_id, limit, logLevel, timestamp) {
         let level = logLevel || default_log_level;
+        if (level === 'default') {
+            level = default_log_level
+        }
+        let nlevel = levels[level];
         return FlowLogs.aggregate([
             { $match: { request_id: request_id } },
             { $unwind: "$rows" },
-            { $match: { "rows.level": level, "rows.timestamp": { $gt: timestamp } } },
+            { $match: { "rows.level": { $lt: nlevel }, "rows.timestamp": { $gt: timestamp } } },
             { $sort: { "rows.timestamp": 1, "rows._id": 1 } },
             { $limit: limit }
         ])
