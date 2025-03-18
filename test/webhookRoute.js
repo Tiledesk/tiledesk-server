fc32dcc599607057ebf422c3338719b65ec88f58
Giovanni Troisi <giovanni@tiledesk.com>
2025-03-18 16:30:34 +0100

    log removed

--- test/webhookRoute.js
+++ test/webhookRoute.js
@@ -358,9 +358,8 @@ describe('WebhookRoute', () => {
                                         if (err) { console.error("err: ", err); }
                                         if (log) { console.log("res.body", res.body); }
 
-                                        console.log("res.body", res.body);
                                         res.should.have.status(200);
-                                        //res.body.should.be.a('object');
+                                        res.body.should.be.a('object');
                                         expect(res.body.success).to.equal(true);
                                         expect(res.body.message).to.equal("Webhook disabled in test mode");
 
