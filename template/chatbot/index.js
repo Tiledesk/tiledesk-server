9271a6b1e5654d24c116b526747ea6252933487d
Giovanni Troisi <giovanni@tiledesk.com>
2025-04-22 16:41:36 +0200

    added official_copilot to chatbot templates index

--- template/chatbot/index.js
+++ template/chatbot/index.js
@@ -3,6 +3,7 @@ module.exports = {
     blank: require('./blank'),
     blank_webhook: require('./blank_webhook'),
     blank_copilot: require('./blank_copilot'),
+    official_copilot: require('./official_copilot'),
     blank_voice: require('./blank_voice'),
     blank_voice_twilio: require('./blank_voice_twilio'),
     empty: require('./empty'),
