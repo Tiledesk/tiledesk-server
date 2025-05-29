08fe866e27e4447b23b67cf22830d9b9b3e1098b
Giovanni Troisi <giovanni@tiledesk.com>
2025-05-29 18:20:21 +0200

    updated twilio-connector to 0.1.22 bug fix on tags scheduling

--- services/Scheduler.js
+++ services/Scheduler.js
@@ -32,12 +32,13 @@ class Scheduler {
     tagSchedule(data, callback) {
 
         winston.debug("(tagScheduler) data: ", data);
-        this.jobManager.publish(data, (err, ok) => {
-            let response_data = { success: true, message: "Scheduled" };
-            if (callback) {
-                callback(err, response_data);
-            }
-        })
+        this.jobManager.publish(data);
+        let response_data = { success: true, message: "Scheduled" };
+        if (callback) {
+            callback(err, response_data);
+        }
+        // this.jobManager.publish(data, (err, ok) => {
+        // })
     }
     
 }
