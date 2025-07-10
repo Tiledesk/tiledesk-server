a216102fb0acfc375e7c13d67e7c55cb723958c6
Gabriele Panico <gabriele.panico@gmail.com>
2025-07-10 10:02:54 +0200

    restore cookie in app.session

--- app.js
+++ app.js
@@ -356,11 +356,11 @@ if (process.env.DISABLE_SESSION_STRATEGY==true ||  process.env.DISABLE_SESSION_S
           resave: false, // required: force lightweight session keep alive (touch)
           saveUninitialized: false, // recommended: only save session when data exists
           secret: sessionSecret,
-          // cookie: {
-          //   secure: true,           // ✅ Serve HTTPS
-          //   httpOnly: true,         // ✅ Solo server-side access
-          //   sameSite: 'None'        // ✅ Permette cross-origin (es. Keycloak su dominio diverso)
-          // }
+          cookie: {
+            secure: true,           // ✅ Use HTTPS
+            httpOnly: true,         // ✅ Only accessible by the server (not client-side JS)
+            sameSite: 'None'        // ✅ Allows cross-origin (e.g., Keycloak on a different domain)
+          }
         })
       )
       winston.info("Express Session with Redis enabled with Secret: " + sessionSecret);
