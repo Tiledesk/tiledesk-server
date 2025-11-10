32c24fdc4036feb0ae3a09ae45dcfa5432a09844
Giovanni Troisi <giovanni@tiledesk.com>
2025-11-10 10:44:37 +0100

    updated webSocketServer

--- websocket/webSocketServer.js
+++ websocket/webSocketServer.js
@@ -90,8 +90,8 @@ class WebSocketServer {
           try {
             console.log("WS Token passed through headers");
             //const htoken = req.headers['sec-websocket-protocol'];
-            const protocol = request.headers[ ' sec-websocket-protocol'];
-            const htoken = Buffer. from(protocol.replace(/-/g, '+').replace(/_/g, '/'), 'base64').toString('utf8');
+            const protocol = info.req.headers[ ' sec-websocket-protocol'];
+            const htoken = Buffer.from(protocol.replace(/-/g, '+').replace(/_/g, '/'), 'base64').toString('utf8');
             token = Array.isArray(htoken) ? htoken[0] : htoken;
             token = token?.replace(/^"|"$/g, '').trim(); // rimuove virgolette e spazi
             console.log("WS Token:", token);
