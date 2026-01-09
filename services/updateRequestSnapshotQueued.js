d612ec64d580e6eeecdd69a76cb4a6ec8f89d6cf
Giovanni Troisi <giovanni@tiledesk.com>
2026-01-09 15:26:50 +0100

    update

--- services/updateRequestSnapshotQueued.js
+++ services/updateRequestSnapshotQueued.js
@@ -16,56 +16,58 @@ class UpdateRequestSnapshotQueued {
   }
 
   updateRequestSnapshot() {
-    console.log("non dovrei fare niente quiiii")
-    return;
-    // let snapshotUpdateKey = 'request.snapshot.update';
-    // if (requestEvent.queueEnabled) {
-    //   snapshotUpdateKey = 'request.snapshot.update.queue';
-    // }
-    // winston.debug("snapshotUpdateKey: " + snapshotUpdateKey);
+    let snapshotUpdateKey = 'request.snapshot.update';
+    if (requestEvent.queueEnabled) {
+      snapshotUpdateKey = 'request.snapshot.update.queue';
+    }
+    winston.debug("snapshotUpdateKey: " + snapshotUpdateKey);
 
-    // requestEvent.on(snapshotUpdateKey, function (data) {
-    //   setImmediate(() => {
-    //     winston.debug("updateRequestSnapshot on request.snapshot.update ", data);
-    //     console.log("updateRequestSnapshot on request.snapshot.update ", data);
+    requestEvent.on(snapshotUpdateKey, function (data) {
+      setImmediate(() => {
+        winston.debug("updateRequestSnapshot on request.snapshot.update ", data);
+        console.log("updateRequestSnapshot on request.snapshot.update ", data);
 
-    //     const request = data.request;
-    //     const snapshot = data.snapshot;
-    //     console.log("(queue) snapshot.requester", snapshot.requester);
+        const request = data.request;
+        const snapshot = data.snapshot;
+        console.log("(queue) updateRequestSnapshot snapshot exists: ", snapshot ? "yes" : "no");
+        console.log("(queue) updateRequestSnapshot snapshot agents length: ", snapshot?.agents?.length);
+        console.log("(queue) updateRequestSnapshot snapshot department exists: ", snapshot.department ? "yes" : "no");
+        console.log("(queue) updateRequestSnapshot snapshot lead exists: ", snapshot.lead ? "yes" : "no");
+        console.log("(queue) updateRequestSnapshot snapshot requester exists: ", snapshot.requester ? "yes" : "no");
 
-    //     if (!request || !request.request_id || !request.id_project) {
-    //       winston.error("updateRequestSnapshot: Invalid request data", data);
-    //       return;
-    //     }
+        if (!request || !request.request_id || !request.id_project) {
+          winston.error("updateRequestSnapshot: Invalid request data", data);
+          return;
+        }
 
-    //     if (!snapshot || Object.keys(snapshot).length === 0) {
-    //       winston.debug("updateRequestSnapshot: Empty snapshot, skipping update");
-    //       return;
-    //     }
+        if (!snapshot || Object.keys(snapshot).length === 0) {
+          winston.debug("updateRequestSnapshot: Empty snapshot, skipping update");
+          return;
+        }
 
-    //     const query = { request_id: request.request_id, id_project: request.id_project };
-    //     winston.debug("updateRequestSnapshot query ", query);
+        const query = { request_id: request.request_id, id_project: request.id_project };
+        winston.debug("updateRequestSnapshot query ", query);
 
-    //     Request.findOneAndUpdate(
-    //       query, 
-    //       { "$set": { "snapshot": snapshot } }, 
-    //       { new: true },
-    //       function (err, updatedRequest) {
-    //         if (err) {
-    //           winston.error("Error updating request snapshot updateRequestSnapshot", err);
-    //           return;
-    //         }
-    //         if (updatedRequest) {
-    //           winston.debug("updateRequestSnapshot updated request " + updatedRequest.request_id);
-    //           requestEvent.emit('request.update.snapshot', { request: updatedRequest, snapshot: snapshot });
-    //         } else {
-    //           winston.warn("updateRequestSnapshot: Request not found for " + request.request_id);
-    //         }
-    //         return;
-    //       }
-    //     );
-    //   });
-    // });
+        Request.findOneAndUpdate(
+          query, 
+          { "$set": { "snapshot": snapshot } }, 
+          { new: true },
+          function (err, updatedRequest) {
+            if (err) {
+              winston.error("Error updating request snapshot updateRequestSnapshot", err);
+              return;
+            }
+            if (updatedRequest) {
+              winston.debug("updateRequestSnapshot updated request " + updatedRequest.request_id);
+              requestEvent.emit('request.update.snapshot', { request: updatedRequest, snapshot: snapshot });
+            } else {
+              winston.warn("updateRequestSnapshot: Request not found for " + request.request_id);
+            }
+            return;
+          }
+        );
+      });
+    });
   }
 }
 
