c0fb299691bffda8b88cbf78455d6e1b763062aa
Giovanni Troisi <giovanni@tiledesk.com>
2026-01-09 17:21:20 +0100

    updated updateRequestSnapshoQueued.js

--- services/updateRequestSnapshotQueued.js
+++ services/updateRequestSnapshotQueued.js
@@ -16,7 +16,7 @@ class UpdateRequestSnapshotQueued {
   }
 
   updateRequestSnapshot() {
-    var snapshotUpdateKey = 'request.snapshot.update';
+    let snapshotUpdateKey = 'request.snapshot.update';
     if (requestEvent.queueEnabled) {
       snapshotUpdateKey = 'request.snapshot.update.queue';
     }
@@ -25,9 +25,15 @@ class UpdateRequestSnapshotQueued {
     requestEvent.on(snapshotUpdateKey, function (data) {
       setImmediate(() => {
         winston.debug("updateRequestSnapshot on request.snapshot.update ", data);
+        console.log("updateRequestSnapshot on request.snapshot.update ", data);
 
-        var request = data.request;
-        var snapshot = data.snapshot;
+        const request = data.request;
+        const snapshot = data.snapshot;
+        console.log("(queue) updateRequestSnapshot snapshot exists: ", snapshot ? "yes" : "no");
+        console.log("(queue) updateRequestSnapshot snapshot agents length: ", snapshot?.agents?.length);
+        console.log("(queue) updateRequestSnapshot snapshot department exists: ", snapshot.department ? "yes" : "no");
+        console.log("(queue) updateRequestSnapshot snapshot lead exists: ", snapshot.lead ? "yes" : "no");
+        console.log("(queue) updateRequestSnapshot snapshot requester exists: ", snapshot.requester ? "yes" : "no");
 
         if (!request || !request.request_id || !request.id_project) {
           winston.error("updateRequestSnapshot: Invalid request data", data);
@@ -39,7 +45,7 @@ class UpdateRequestSnapshotQueued {
           return;
         }
 
-        var query = { request_id: request.request_id, id_project: request.id_project };
+        const query = { request_id: request.request_id, id_project: request.id_project };
         winston.debug("updateRequestSnapshot query ", query);
 
         Request.findOneAndUpdate(
@@ -53,7 +59,6 @@ class UpdateRequestSnapshotQueued {
             }
             if (updatedRequest) {
               winston.debug("updateRequestSnapshot updated request " + updatedRequest.request_id);
-              requestEvent.emit('request.update.snapshot', { request: updatedRequest, snapshot: snapshot });
             } else {
               winston.warn("updateRequestSnapshot: Request not found for " + request.request_id);
             }
