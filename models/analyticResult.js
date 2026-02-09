aa1e00133120a5a87a13063ceab45517d9556128
Giovanni Troisi <giovanni@tiledesk.com>
2026-02-09 13:32:28 +0100

    deleted double declaration

--- models/analyticResult.js
+++ models/analyticResult.js
@@ -4,9 +4,6 @@ var Schema = mongoose.Schema;
 var winston = require('../config/winston');
 let config = require('../config/database');
 
-let config = require('../config/database');
-
-
 var databaseUri = process.env.DATABASE_URI || process.env.MONGODB_URI || config.database;
 
 var readPreference = process.env.ANALYTICS_READ_PREFERENCE ||  "primary";
