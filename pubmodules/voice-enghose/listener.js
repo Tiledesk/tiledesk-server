6a01b270eb96c7632ef521b7659fa85776502646
Gabriele Panico <gabriele.panico@gmail.com>
2026-02-12 16:55:31 +0100

    added: tiledesk-vxml-enghouse-connector route

---
+++ pubmodules/voice-enghose/listener.js
@@ -0,0 +1,66 @@
+const voice_twilio = require('@tiledesk/tiledesk-voice-twilio-connector');
+let winston = require('../../config/winston');
+let configGlobal = require('../../config/global');
+const mongoose = require('mongoose');
+
+const apiUrl = process.env.API_URL || configGlobal.apiUrl;
+winston.info("VoiceEnghouse apiUrl: " + apiUrl);
+
+const dbConnection = mongoose.connection;
+
+class Listener {
+
+    listen(config) {
+
+        winston.info("VoiceEnghouse Listener listen");
+        if (config.databaseUri) {
+            winston.debug("VoiceEnghouse config databaseUri: " +  config.databaseUri);
+        }
+
+        var port = process.env.CACHE_REDIS_PORT || 6379;
+        winston.debug("Redis port: "+ port);
+
+        var host = process.env.CACHE_REDIS_HOST || "127.0.0.1"
+        winston.debug("Redis host: "+ host);
+
+        var password = process.env.CACHE_REDIS_PASSWORD;
+        winston.debug("Redis password: "+ password);
+
+        let brand_name = null;
+        if (process.env.BRAND_NAME) {
+            brand_name = process.env.BRAND_NAME
+        }
+
+        const baseUrl = apiUrl + "/modules/voice-enghouse";
+        winston.debug("Voice Enghouse baseUrl: "+ baseUrl);
+
+        const relativeBaseUrl = process.env.API_ENDPOINT + "/modules/voice-enghouse";
+        winston.debug("Voice Enghouse relativeBaseUrl: "+ relativeBaseUrl);
+
+        let log = process.env.VOICE_TWILIO_LOG || false
+        winston.debug("Voice log: "+ log);
+        
+        voice_twilio.init({
+            MONGODB_URI: config.databaseUri,          
+            dbconnection: dbConnection,
+            BASE_URL: baseUrl,
+            RELATIVE_BASE_URL: relativeBaseUrl,
+            BASE_FILE_URL: apiUrl,                     
+            REDIS_HOST: host,
+            REDIS_PORT: port,
+            REDIS_PASSWORD: password,
+            BRAND_NAME: brand_name,
+            log: log
+        }, (err) => {
+            if (!err) {
+                winston.info("Tiledesk Voice Enghouse Connector proxy server succesfully started.");
+            } else {
+                winston.info("unable to start Tiledesk Voice Enghouse Connector. " + err);
+            }    
+        })
+    }
+}
+
+let listener = new Listener();
+
+module.exports = listener;
