528d87594aa75acb5be256c016da73b7e263f55d
Giovanni Troisi <giovanni@tiledesk.com>
2026-05-11 14:45:30 +0200

    Refine duration tracking in Q&A endpoint of kb.js
    - Removed total_duration variable and adjusted duration calculation to return seconds instead of milliseconds for improved clarity in response times.
    - Updated response object to eliminate total_duration from the response, streamlining the data sent back to the client.

--- routes/kb.js
+++ routes/kb.js
@@ -364,8 +364,6 @@ router.post('/qa', async (req, res) => {
   let publicKey = false;
   let data = req.body;
 
-  let total_duration = Date.now();
-
   let namespace;
   try {
     namespace = await aiManager.checkNamespace(id_project, data.namespace);
@@ -612,7 +610,7 @@ router.post('/qa', async (req, res) => {
     winston.debug("qa resp: ", resp.data);
     let answer = resp.data;
 
-    answer.duration = Date.now() - duration;
+    answer.duration = (Date.now() - duration) / 1000;
 
     if (publicKey === true) {
       let modelKey;
@@ -635,7 +633,6 @@ router.post('/qa', async (req, res) => {
       winston.verbose("incremented_key: ", incremented_key);
     }
   
-    answer.total_duration = Date.now() - total_duration;
     return res.status(200).send(answer);
 
   }).catch((err) => {
